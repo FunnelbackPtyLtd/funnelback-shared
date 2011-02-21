63626d7434acac909efce2db78b33e064561e3ae
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-02-21 06:20:11 +0000

    Deleted old files
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@22672 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- src/com/funnelback/publicui/search/model/padre/ResultType.java
+++
@@ -1,9 +0,0 @@
-package com.funnelback.publicui.search.model.padre;
-
-/**
- * Common interface for {@link Result} and {@link TierBar}
- * to permit having heterogeneous lists.
- */
-public interface ResultType {
-
-}
--- test_data/dummy-search_home/conf/faceted-navigation/faceted_navigation_transform.groovy
+++
@@ -1,5 +0,0 @@
-import com.funnelback.publicui.search.model.transaction.*
-
-for (Facet f: facets) {
-    f.name = f.name + " (Groovy)"
-}
