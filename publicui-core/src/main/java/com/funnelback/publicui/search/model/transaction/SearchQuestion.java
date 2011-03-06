4afefbae2cfaf0a48377b93b66be89e3f164efc8
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-03-06 23:23:32 +0000

    Fixing dependencies shared between both projects
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@22860 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/SearchQuestion.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/SearchQuestion.java
@@ -13,9 +13,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import com.funnelback.common.config.DefaultValues;
-import com.funnelback.publicui.search.lifecycle.input.InputProcessor;
 import com.funnelback.publicui.search.model.collection.Collection;
-import com.funnelback.publicui.search.service.log.LogUtils;
 
 @ToString
 @AllArgsConstructor
