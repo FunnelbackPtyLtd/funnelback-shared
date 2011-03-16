d9a0f8f53b59472f1b3168f08c8f81713252b730
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-03-16 06:46:36 +0000

    Added POC with "Embedded JS" templating engine
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@23010 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/resources/bootstrap.js
@@ -0,0 +1,8 @@
+(function() {
+	load('file:///C:/Data/dev/eclipse-workspace/publicui-parent/publicui-web/src/main/resources/env.rhino.1.2.js');
+	// load('file:///C:/Data/dev/eclipse-workspace/publicui-parent/publicui-web/src/main/resources/jquery-1.5.1.js');
+
+	window.location='file:///C:/Data/dev/eclipse-workspace/publicui-parent/publicui-web/src/main/resources/envJs-jQuery.html';
+	
+	
+}) ();
