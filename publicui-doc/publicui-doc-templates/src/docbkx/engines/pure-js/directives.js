472caaf3d609a881ea831f0527e6f6573f7aaa97
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-03-17 06:44:22 +0000

    Added a bit more doco about templating engines
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@23028 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-doc/publicui-doc-templates/src/docbkx/engines/pure-js/directives.js
@@ -0,0 +1,19 @@
+'ol#fb-results li.result' : {
+	'result<-SearchTransaction.response.resultPacket.results' : {
+		'h3 span.fb-filetype' : 'result.fileType',
+		'h3 a@href' : 'result.displayUrl',
+		'h3 a' : 'result.title',
+		
+		'p span.fb-summary' : 'result.summary',
+		
+		// The following one maps a function to a directive.
+		// It's the only way to extend the engine
+		'p span.fb-date' : function (arg) {
+				return new Date(arg.item.date).toUTCString();
+			},
+		
+		'cite' : 'result.displayUrl',
+		
+		'a.fb-cached@href' : 'result.cacheUrl'
+	}
+}
