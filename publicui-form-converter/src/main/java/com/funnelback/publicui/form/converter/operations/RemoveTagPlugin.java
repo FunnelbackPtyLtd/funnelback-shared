72ba72b5a08e3bdcfbbfa8cb79d8c98f0edefa9f
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-04-12 04:53:08 +0000

    Added step to remove existing Tagging plugins.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@23344 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-form-converter/src/main/java/com/funnelback/publicui/form/converter/operations/RemoveTagPlugin.java
@@ -0,0 +1,40 @@
+package com.funnelback.publicui.form.converter.operations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import lombok.extern.apachecommons.Log;
+
+import com.funnelback.publicui.form.converter.Operation;
+
+/**
+ * Removes tagging plugins. Tagging is not supported (yet)
+ * in the Java UI so tags are removed for now.
+ */
+@Log
+public class RemoveTagPlugin implements Operation {
+
+	private static final Pattern TAG_LINK_PATTERN = Pattern.compile("([\\s\\t]*)?<s:ResPlugin\\s+name=['\"]ResultTagLink['\"]\\s*>.*?</s:ResPlugin>([\\s\\t]*)?", Pattern.DOTALL);
+	private static final Pattern TAG_LIST_PATTERN = Pattern.compile("([\\s\\t]*)?<s:ResPlugin\\s+name=['\"]ResultTagList['\"]\\s*>.*?</s:ResPlugin>([\\s\\t]*)?", Pattern.DOTALL);
+	
+	@Override
+	public String process(String in) {
+		
+		String out = in;
+
+		Matcher m = TAG_LINK_PATTERN.matcher(out);
+		if (m.find()) {
+			out = m.replaceAll("");
+			log.warn("Tagging link plugin tag(s) ('ResultTagLink') have been removed.");
+		}
+
+		m = TAG_LIST_PATTERN.matcher(out);
+		if (m.find()) {
+			out = m.replaceAll("");
+			log.warn("Tagging list plugin tag(s) ('ResultTagList') have been removed.");
+		}
+				
+		return out;
+	}
+
+}
