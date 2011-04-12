2fd193185fbcf37c4dca0caaa1f2bce7aff71641
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-04-12 04:43:53 +0000

    Added conversion of <TierBarBestBets> tags
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@23342 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-form-converter/src/main/java/com/funnelback/publicui/form/converter/operations/BestBetsConversion.java
+++ publicui-form-converter/src/main/java/com/funnelback/publicui/form/converter/operations/BestBetsConversion.java
@@ -13,6 +13,9 @@ import com.funnelback.publicui.form.converter.Operation;
 @Log
 public class BestBetsConversion implements Operation {
 
+	private static final String DEFAULT_TABLE_ATTR = "'bgcolor=\"#CCCCCC\" summary=\"Result header\" width=\"100%\" cellpadding=4 cellspacing=0";
+	private static final String DEFAULT_HTML = "<p><table "+DEFAULT_TABLE_ATTR+"><tr><td><b>Best bets</b></td></tr></table></p>";
+
 	@Override
 	public String process(String in) {
 		
@@ -37,6 +40,19 @@ public class BestBetsConversion implements Operation {
 		
 		// Replace data tags
 		out = out.replaceAll("bb\\{bb_(\\w*)\\}", "\\${s.bb.$1}");
+		
+		// Capture TierBar HTML fragment
+		// Try to capture TierBar HTML fragments
+		String htmlFragment = DEFAULT_HTML;
+		m = Pattern.compile("<s:TierBarBestBets>(.*?)</s:TierBarBestBets>", Pattern.DOTALL).matcher(out);
+		if (m.find()) {
+			htmlFragment = m.group(1);	
+			// Remove old tier bar tag
+			out = m.replaceAll("");
+		}
+		// Insert HTML fragment after the <@s.BestBet> tag
+		out = out.replace("<@s.BestBets>", "<@s.BestBets>" + htmlFragment);
+
 
 		return out;
 	}
