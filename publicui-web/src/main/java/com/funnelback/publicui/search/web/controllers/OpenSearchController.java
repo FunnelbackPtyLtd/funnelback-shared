d051af3ac005917e321759bd36c818743930e6e3
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-05-05 02:01:25 +0000

    Finally got this OpenSearch thing working
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@23729 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/OpenSearchController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/OpenSearchController.java
@@ -8,41 +8,52 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.DataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 
 import com.funnelback.common.config.Keys;
 import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.service.ConfigRepository;
-import com.funnelback.publicui.search.web.binding.CollectionEditor;
 
+/**
+ * Generate an OpenSearchDescription XML snippet
+ * for a given collection. 
+ */
 @Controller
 public class OpenSearchController {
 
+	private static final String URI = "open-search.xml";
+	
 	@Autowired
 	private ConfigRepository configRepository;
-
-	@InitBinder
-	public void initBinder(DataBinder binder) {
-		binder.registerCustomEditor(Collection.class, new CollectionEditor(configRepository));
-	}
-
 	
-	@RequestMapping("/open-search.xml")
-	public ModelAndView openSearch(Collection collection, HttpServletRequest request, HttpServletResponse response) {
+	@Autowired
+	private FreeMarkerView openSearchView;
+
+	@RequestMapping(value="/"+URI,params=RequestParameters.COLLECTION)
+	public ModelAndView openSearch(HttpServletRequest request, HttpServletResponse response) {
+		Collection collection = configRepository.getCollection(request.getParameter(RequestParameters.COLLECTION));
 		if (collection != null) {
 			Map<String, String> model = new HashMap<String, String>();
 			model.put("serviceName", collection.getConfiguration().value(Keys.SERVICE_NAME));
 			model.put("name", collection.getId());
-			model.put("host", request.getLocalName());
-			model.put("searchUrl", "dummy");
-			return new ModelAndView("open-search", model);
+			model.put("host", request.getHeader("Host"));
+			model.put("searchUrl", buildSearchUrl(request, collection));
+			return new ModelAndView(openSearchView, model);
 		} else {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return null;
 		}
+	}
+	
+	private String buildSearchUrl(HttpServletRequest request, Collection collection) {
+		return new StringBuffer()
+		.append(request.getRequestURL().toString().replace(URI, collection.getConfiguration().value(Keys.PublicUI.SEARCH_LINK)))
+		.append("?collection=" + collection.getId())
+		.append("&amp;query={searchTerms}").toString();
+		
 	}
 	
 }
