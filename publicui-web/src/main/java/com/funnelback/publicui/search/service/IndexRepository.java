81cdddaa830c0528d0c4016443c835d836b238cb
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-07-22 06:48:56 +0000

    Fixed bad key used in test to retrieve indexer arguments
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@25292 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/service/IndexRepository.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/IndexRepository.java
@@ -15,7 +15,9 @@ public interface IndexRepository {
 		/** PADRE version string (Starting with 'version') */
 		version,
 		/** List of indexer arguments, separated by {@link #INDEXER_OPTIONS_SEPARATOR} */
-		indexer_arguments
+		indexer_arguments,
+		/** Numbers of documents in the collection */
+		Num_docs
 	}
 	
 	/** Separator for the indexer options of the <code>.bldinfo</code> file */
@@ -24,13 +26,17 @@ public interface IndexRepository {
 	/**
 	 * @param c
 	 * @return The last update date for the collection
+	 * @throws UnsupportedOperationException if the collection is a meta collection,
+	 * since they don't have a <code>index_time</code> file.
 	 */
-	public Date getLastUpdated(String collectionId);
+	public Date getLastUpdated(String collectionId) throws UnsupportedOperationException;
 	
 	/**
 	 * Retrieve a value from the <code>.bldinfo<code> file.
 	 * @param key
 	 * @return
+	 * @throws UnsupportedOperationException if the collection is a meta collection,
+	 * since they don't have a <code>.bldinfo</code> file.
 	 */
-	public String getBuildInfoValue(String collectionId, String key);
+	public String getBuildInfoValue(String collectionId, String key) throws UnsupportedOperationException;
 }
