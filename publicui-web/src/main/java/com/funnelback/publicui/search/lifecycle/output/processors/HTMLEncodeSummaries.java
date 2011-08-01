b5580f12bde689c92e6abe2454dc9b3ad2d41fa2
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-08-01 06:04:09 +0000

    [FUN-3417] [FUN-3530] Removed code that deals with PADRE highlighting now that it doesn't highlight anymore
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@25438 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/HTMLEncodeSummaries.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/HTMLEncodeSummaries.java
@@ -10,25 +10,18 @@ import com.funnelback.publicui.search.model.transaction.SearchTransaction;
 import com.funnelback.publicui.search.model.transaction.SearchTransactionUtils;
 
 /**
- * <p>HTML Encodes summaries returned by PADRE, then re-decodes
- * the highlight tags (&lt;b&gt;).</p>
- * 
- * <p>This can be removed once that FUN-3530 is fixed.</p>
- *
+ * <p>HTML Encodes summaries returned by PADRE because they
+ * can contain decoded HTML entities (See FUN-3530).
  */
 @Component("htmlEncodeSummaries")
 public class HTMLEncodeSummaries implements OutputProcessor {
 
-	private static final String PADRE_HIGHLIGHT_TAG = "strong";
-	
 	@Override
 	public void processOutput(SearchTransaction searchTransaction) throws OutputProcessorException {
 		if (SearchTransactionUtils.hasResults(searchTransaction)) {
 			for(Result r: searchTransaction.getResponse().getResultPacket().getResults()) {
 				if (r.getSummary() != null) {
-					r.setSummary(HtmlUtils.htmlEscape(r.getSummary())
-							.replace("&lt;"+PADRE_HIGHLIGHT_TAG+"&gt;", "<"+PADRE_HIGHLIGHT_TAG + ">")
-							.replace("&lt;/"+PADRE_HIGHLIGHT_TAG+"&gt;", "</"+PADRE_HIGHLIGHT_TAG+">"));
+					r.setSummary(HtmlUtils.htmlEscape(r.getSummary()));
 				}
 			}
 		}
