023693adf92f8f7527d1d4087caeebc2afc0d097
Tim Jones <tjones@funnelback.com>
2011-08-01 05:23:52 +0000

    Changed the MetaInfo class to (hopefully) allow xstream deserialisation on Centos.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@25428 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/contentoptimiser/processors/impl/MetaInfo.java
+++ publicui-web/src/main/java/com/funnelback/contentoptimiser/processors/impl/MetaInfo.java
@@ -1,12 +1,15 @@
 package com.funnelback.contentoptimiser.processors.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MetaInfo {
-		@Getter private final String shortTitle;
-		@Getter private final String longTitle;
-		@Getter private final String improvementSuggestion;
-		@Getter private final Integer threshold;
+		@Getter @Setter private  String shortTitle;
+		@Getter @Setter private  String longTitle;
+		@Getter @Setter private  String improvementSuggestion;
+		@Getter @Setter private  Integer threshold;
+		
+		public MetaInfo() {} // for VMs that only support pure java xstream
 }
