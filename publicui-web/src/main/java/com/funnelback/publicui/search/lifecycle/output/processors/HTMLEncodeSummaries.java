08139e15ed8315ca596b97709d108106cf1fda6e
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-08-08 00:57:10 +0000

    Fixed HTML encoding: Encode only some specific HTML entities that could be interpreted on the UI side, but let the others as is (accented letters, etc)
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@25603 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/HTMLEncodeSummaries.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/HTMLEncodeSummaries.java
@@ -1,5 +1,6 @@
 package com.funnelback.publicui.search.lifecycle.output.processors;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.HtmlUtils;
 
@@ -11,10 +12,16 @@ import com.funnelback.publicui.search.model.transaction.SearchTransactionUtils;
 
 /**
  * <p>HTML Encodes summaries returned by PADRE because they
- * can contain decoded HTML entities (See FUN-3530).
+ * can contain decoded HTML entities (See FUN-3530).</p>
+ * 
+ * <p>Only encode specific unsafe characters that could generate
+ * invalid HTML. Other entities (accented letters, etc.)</p>
  */
 @Component("htmlEncodeSummaries")
 public class HTMLEncodeSummaries implements OutputProcessor {
+	
+	private static final String[] UNSAFE_CHARS = {"\"", "\"", "'", "<", ">", "&"};
+	private static final String[] SAFE_REPLACEMENT = {"&#92;", "&quot;", "&apos;", "&lt;", "&gt;", "&amp;"};
 
 	@Override
 	public void processOutput(SearchTransaction searchTransaction) throws OutputProcessorException {
@@ -22,6 +29,7 @@ public class HTMLEncodeSummaries implements OutputProcessor {
 			for(Result r: searchTransaction.getResponse().getResultPacket().getResults()) {
 				if (r.getSummary() != null) {
 					r.setSummary(HtmlUtils.htmlEscape(r.getSummary()));
+					StringUtils.replaceEach(r.getSummary(), UNSAFE_CHARS, SAFE_REPLACEMENT);
 				}
 			}
 		}
