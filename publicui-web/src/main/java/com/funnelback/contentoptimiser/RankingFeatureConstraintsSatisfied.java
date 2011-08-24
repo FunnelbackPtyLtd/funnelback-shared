cd880ef78c7923b80912c1b324c0ef9139ab01aa
Tim Jones <tjones@funnelback.com>
2011-08-24 06:59:33 +0000

    Added missing file for the last content optimiser fix
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@26000 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/contentoptimiser/RankingFeatureConstraintsSatisfied.java
@@ -0,0 +1,18 @@
+package com.funnelback.contentoptimiser;
+
+import com.funnelback.publicui.search.model.padre.ResultPacket;
+import com.funnelback.publicui.search.model.transaction.contentoptimiser.RankingFeature;
+
+public class RankingFeatureConstraintsSatisfied extends RankingFeature {
+
+	public RankingFeatureConstraintsSatisfied(String name, String category, ResultPacket rp) {
+		super(name, category, "Query constraints");
+		
+	}
+
+	@Override
+	public void caculateWin(float selectedScore, float weighting) {
+		this.win = 100 * weighting;
+	}
+
+}
