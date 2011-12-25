49c5a08996f5eb4b3e7037d1b61e0e9c37804fef
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-12-25 16:02:59 +0000

    [FUN-4011] Added support to configure FreeMarker error handling, to avoid failing completely to render a template if there is a runtime error. Parsing errors are left as is as they should only occur during development and *must* be seen by the developer
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@27333 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/exception/DefaultTemplateExceptionHandler.java
@@ -0,0 +1,82 @@
+package com.funnelback.publicui.search.web.exception;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import lombok.extern.apachecommons.CommonsLog;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.funnelback.common.config.DefaultValues;
+import com.funnelback.common.config.DefaultValues.ModernUI.ErrorFormat;
+import com.funnelback.common.config.Keys;
+import com.funnelback.publicui.i18n.I18n;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.web.controllers.SearchController;
+
+import freemarker.core.Environment;
+import freemarker.template.AdapterTemplateModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.TemplateHashModel;
+
+/**
+ * <p>Customise the way FreeMarker template exceptions are handled.</p>
+ * 
+ * <p>This handler will simply log the error but won't fail the whole page
+ * rendering.</p>
+ *
+ * @since v11.4
+ */
+@Component
+@CommonsLog
+public class DefaultTemplateExceptionHandler implements TemplateExceptionHandler {
+
+	@Autowired
+	private I18n i18n;
+	
+	@Override
+	public void handleTemplateException(TemplateException te, Environment env, Writer out) throws TemplateException {
+		log.error("An error occured while processing a template", te);
+		
+		// Try to get the source collection config
+		TemplateHashModel model = env.getDataModel();
+		if (model.get(SearchController.ModelAttributes.question.toString()) != null) {
+			SearchQuestion sq = (SearchQuestion) ((AdapterTemplateModel)model.get(SearchController.ModelAttributes.question.toString())).getAdaptedObject(SearchQuestion.class);
+			boolean displayErrors = sq.getCollection().getConfiguration().valueAsBoolean(
+					Keys.ModernUI.FREEMARKER_DISPLAY_ERRORS,
+					DefaultValues.ModernUI.FREEMARKER_DISPLAY_ERRORS);
+			if (displayErrors) {
+				String errorFormat = sq.getCollection().getConfiguration().value(
+						Keys.ModernUI.FREEMARKER_ERROR_FORMAT,
+						DefaultValues.ModernUI.ErrorFormat.exception.toString());
+				try {
+					ErrorFormat fmt = ErrorFormat.valueOf(errorFormat);
+					switch (fmt) {
+					case exception:
+						out.write(te.getFTLInstructionStack());
+						throw te;
+					case html:
+						out.write("<!-- " + i18n.tr("freemarker.template.error") + ": " + te.getMessage() + " -->\n");
+						break;
+					case json:
+						out.write("/* " + i18n.tr("freemarker.template.error") + ": " + te.getMessage() + " */\n");
+						break;
+					case string:
+						out.write(i18n.tr("freemarker.template.error") + ": " + te.getMessage());
+						break;
+					}
+				} catch (IOException ioe) {
+					throw new TemplateException(ioe, env);
+				} catch (IllegalArgumentException iae) {
+					log.error(i18n.tr("freemarker.error_format.invalid", errorFormat, StringUtils.join(ErrorFormat.values(), ",")));
+					throw new TemplateException(iae, env);
+				}
+			}
+		} else {
+			throw te;
+		}
+	}
+}
---
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/web/exception/DefaultTemplateExceptionHandlerTest.java
@@ -0,0 +1,122 @@
+package com.funnelback.publicui.test.search.web.exception;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.funnelback.common.config.Config;
+import com.funnelback.common.config.Keys;
+import com.funnelback.common.config.NoOptionsConfig;
+import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import com.funnelback.publicui.search.web.exception.DefaultTemplateExceptionHandler;
+
+import freemarker.core.Environment;
+import freemarker.ext.beans.BeanModel;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.SimpleHash;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateHashModel;
+import freemarker.template.TemplateModel;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/test/resources/spring/applicationContext.xml")
+public class DefaultTemplateExceptionHandlerTest {
+
+	@Autowired
+	private DefaultTemplateExceptionHandler handler;
+	
+	private Environment env;
+	private TemplateModel model;
+	private Config config;
+	private StringWriter out;
+	@Before
+	public void before() throws IOException {
+		
+		config = new NoOptionsConfig(new File("src/test/resources/dummy-search_home"), "dummy");
+		
+		SearchQuestion sq = new SearchQuestion();
+		sq.setCollection(new Collection("dummy", config));
+		SearchTransaction st = new SearchTransaction(sq, null);
+		model = new BeanModel(st, new DefaultObjectWrapper());
+		
+		out = new StringWriter();
+		env = new Environment(new Template("", new StringReader(""), null), (TemplateHashModel) model, out);
+		
+
+	}
+	
+	@Test
+	public void testNoSearchQuestion() {
+		model = new SimpleHash();
+		try {
+			handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+			Assert.fail();
+		} catch (TemplateException te) { }		
+	}
+	
+	@Test
+	public void testDefaultConfig() {
+		try {
+			handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+			Assert.fail();
+		} catch (TemplateException te) { }		
+
+		Assert.assertFalse(out.getBuffer().toString().equals(""));	
+	}
+
+	@Test
+	public void testInvalidFormat() {
+		config.setValue(Keys.ModernUI.FREEMARKER_ERROR_FORMAT, "invalid");
+		try {
+			handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+			Assert.fail();
+		} catch (TemplateException te) {
+			Assert.assertEquals(te.getCause().getClass(), IllegalArgumentException.class);
+		}		
+
+		Assert.assertTrue(out.getBuffer().toString().equals(""));
+	}
+	
+	@Test
+	public void testHtml() throws TemplateException {
+		config.setValue(Keys.ModernUI.FREEMARKER_ERROR_FORMAT, "html");
+		handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+		
+		Assert.assertFalse(out.getBuffer().toString().equals(""));
+		Assert.assertTrue(out.getBuffer().toString().startsWith("<!--"));
+		Assert.assertTrue(out.getBuffer().toString().endsWith("-->\n"));
+		Assert.assertTrue(out.getBuffer().toString().contains("TPL_ERROR"));
+	}
+
+	@Test
+	public void testJson() throws TemplateException {
+		config.setValue(Keys.ModernUI.FREEMARKER_ERROR_FORMAT, "json");
+		handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+		
+		Assert.assertFalse(out.getBuffer().toString().equals(""));
+		Assert.assertTrue(out.getBuffer().toString().startsWith("/*"));
+		Assert.assertTrue(out.getBuffer().toString().endsWith("*/\n"));
+		Assert.assertTrue(out.getBuffer().toString().contains("TPL_ERROR"));
+	}
+
+	@Test
+	public void testString() throws TemplateException {
+		config.setValue(Keys.ModernUI.FREEMARKER_ERROR_FORMAT, "json");
+		handler.handleTemplateException(new TemplateException("TPL_ERROR", env), env, out);
+		
+		Assert.assertFalse(out.getBuffer().toString().equals(""));
+		Assert.assertTrue(out.getBuffer().toString().contains("TPL_ERROR"));
+	}
+}
