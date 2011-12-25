5fa88604df27512c29e86cfa48108d065fddeca1
Nicolas Guillaumin <nguillaumin@funnelback.com>
2011-12-25 16:05:38 +0000

    Baby steps to re-implement cache.cgi
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@27334 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/CacheController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/CacheController.java
@@ -1,16 +1,22 @@
 package com.funnelback.publicui.search.web.controllers;
 
-import java.io.IOException;
+import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.funnelback.common.config.Config;
+import com.funnelback.common.config.DefaultValues;
+import com.funnelback.common.config.Keys;
 import com.funnelback.publicui.search.model.collection.Collection;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.service.ConfigRepository;
@@ -34,19 +40,42 @@ public class CacheController {
 	}
 
 	@RequestMapping(value="/cache", method=RequestMethod.GET)
-	public void cache(HttpServletResponse response,
+	public ModelAndView cache(HttpServletRequest request,
+			HttpServletResponse response,
 			@RequestParam(RequestParameters.COLLECTION) Collection collection,
-			String doc) throws IOException {
+			String doc,
+			String url) throws Exception {
 		
-		String content = dataRepository.getCachedDocument(collection, doc);
-		if (content != null) {
-			// TODO For now response as text/plain
-			response.setContentType("text/plain");
-			response.getWriter().write(content);
-		} else {
-			// Cached copy unavailable
-			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		if (cachedCopiesDisabled(collection.getConfiguration()) || doc == null) {
+			return new ModelAndView(DefaultValues.FOLDER_WEB+"/"
+					+DefaultValues.FOLDER_TEMPLATES+"/"
+					+DefaultValues.FOLDER_MODERNUI+"/cached-copy-unavailable",
+					new HashMap<String, Object>());
+		} else {		
+			String content = dataRepository.getCachedDocument(collection, doc);
+			if (content != null) {
+				// TODO For now response as text/plain
+				response.setContentType("text/plain");
+				response.getWriter().write(content);
+			} else {
+				// Cached copy unavailable
+				response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+			}
+			return null;
 		}
+	}
+	
+	/**
+	 * Test if cached copies are available or not depending of the
+	 * collection configuration
+	 * @param c
+	 * @return
+	 */
+	private boolean cachedCopiesDisabled(Config c) {
+		return c.valueAsBoolean(Keys.UI_CACHE_DISABLED)
+				|| c.hasValue(Keys.SecurityEarlyBinding.USER_TO_KEY_MAPPER)
+				|| ( c.hasValue(Keys.DocumentLevelSecurity.DOCUMENT_LEVEL_SECURITY_MODE)
+						&& Config.isTrue(c.value(Keys.DocumentLevelSecurity.DOCUMENT_LEVEL_SECURITY_MODE)));
 	}
 	
 }
