45af9b3ba372f8ded5736698d411eb57f91e92b0
Matthew Sheppard <msheppard@funnelback.com>
2012-07-30 02:00:20 +0000

    [FUN-4214] Initial work on pulling the url rendering logic out into spring components.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@30860 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/image/DefaultUrlRenderer.java
@@ -0,0 +1,106 @@
+package com.funnelback.publicui.search.service.image;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import lombok.Cleanup;
+import lombok.extern.log4j.Log4j;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Log4j
+@Component
+public class DefaultUrlRenderer implements UrlRenderer {
+	
+	protected static final String CACHE = DefaultUrlRenderer.class.getSimpleName() + "Repository";
+
+	@Autowired
+	protected CacheManager appCacheManager;
+
+	@Autowired
+	private File searchHome;
+
+	public byte[] fetch(String url) throws IOException {
+		
+		Cache cache = appCacheManager.getCache(CACHE);
+
+		if (! cache.isKeyInCache(url)) {
+			log.trace("Fetching " + url + " to cache");
+
+			URL imageUrl = new URL(url);
+			@Cleanup InputStream imageStream = imageUrl.openStream();
+			cache.put(new Element(url, IOUtils.toByteArray(imageStream)));
+		}
+
+		return (byte[])cache.get(url).getValue();
+	}
+
+	@Override
+	public byte[] renderUrl(String url, int width, int height)
+			throws IOException {
+		Cache cache = appCacheManager.getCache(CACHE);
+
+		String key = url;
+		
+		if (! cache.isKeyInCache(key)) {
+			File phantomBinary;
+			
+			boolean isWindows = System.getProperty("os.name").toLowerCase().indexOf("win") >= 0;
+			boolean isMac = System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0;
+			boolean is64Bit = false;
+			
+			if (isMac) {
+				phantomBinary = new File(searchHome, "mbin" + File.separator + "phantomjs" + File.separator + "bin" + File.separator + "phantomjs");
+			} else if (isWindows) {
+				phantomBinary = new File(searchHome, "wbin" + File.separator + "phantomjs" + File.separator + "phantomjs.exe");
+			} else {
+				if (is64Bit) {
+					phantomBinary = new File(searchHome, "linbin" + File.separator + "phantomjs" + File.separator + "64" + File.separator + "bin" + File.separator + "phantomjs");
+				} else {
+					phantomBinary = new File(searchHome, "linbin" + File.separator + "phantomjs" + File.separator + "32" + File.separator + "bin" + File.separator + "phantomjs");					
+				}
+			}
+
+			File phantomPreview = new File(searchHome, "bin" + File.separator + "phantom_preview.js");
+
+			File tempFile = null;
+			try {
+				tempFile = File.createTempFile("phantom_preview", ".png");
+				
+				CommandLine cmdLine = new CommandLine(phantomBinary.getAbsolutePath());
+				cmdLine.addArgument(phantomPreview.getAbsolutePath());
+				cmdLine.addArgument(url);
+				cmdLine.addArgument(Integer.toString(width));
+				cmdLine.addArgument(Integer.toString(height));
+				cmdLine.addArgument(tempFile.getCanonicalPath());
+				DefaultExecutor executor = new DefaultExecutor();
+	
+				int result = executor.execute(cmdLine);
+				if (result != 0) {
+					throw new RuntimeException(cmdLine.toString() + " failed with code " + result);
+				}
+		
+				byte[] imageBytes = FileUtils.readFileToByteArray(tempFile);
+
+				cache.put(new Element(key, imageBytes));
+			} finally {
+				if (tempFile != null && tempFile.exists()) {
+					tempFile.delete();
+				}
+			}
+			
+		}
+		
+		return (byte[])cache.get(key).getValue();
+	}
+}
---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/image/UrlRenderer.java
@@ -0,0 +1,8 @@
+package com.funnelback.publicui.search.service.image;
+
+import java.io.IOException;
+
+public interface UrlRenderer {
+	byte[] renderUrl(String url, int width, int height) throws IOException;
+}
+
