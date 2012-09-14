f1e011f3e199a143231fe9891e14f332320e6062
Cliff Henderson <chenderson@funnelback.com>
2012-09-14 06:16:45 +0000

    FUN-4966 Novell selenium test failing
    Initial commit of KeyMapper for Novell logins
    
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@31701 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/NovellMapper.java
@@ -0,0 +1,58 @@
+/*
+ * COPYRIGHT (C) 2012 Funnelback
+ */
+package com.funnelback.publicui.search.lifecycle.input.processors.userkeys;
+
+
+import com.funnelback.jetty.jaas.session.UserKeys;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import java.security.Principal;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import javax.security.auth.Subject;
+import org.eclipse.jetty.plus.jaas.JAASUserPrincipal;
+
+/**
+ * Novell key mapper.  Returns set of user and group keys read from Novell
+ * eDirectory server.
+ * 
+ * @author cliff
+ *
+ * $Id:$
+ */
+public class NovellMapper implements UserKeysMapper {
+    
+    /**
+     * Get Novell eDirectory user and group keys for current transaction.
+     * 
+     * @returns List of user and group keys formatted for padre key/lock processing.
+     *          If none, returns an empty list. 
+     */
+    @Override
+    public List<String> getUserKeys(SearchTransaction transaction) {
+        Principal principal = transaction.getQuestion().getPrincipal();
+        List<String> keys = new LinkedList<>();
+        if (null == principal) {
+            throw new NullPointerException("Null principal in search transaction: "+
+                    transaction.toString());
+        }
+        
+        if (principal.getClass().equals(JAASUserPrincipal.class)) {
+            JAASUserPrincipal jp = (JAASUserPrincipal) principal;
+            Subject subject = jp.getSubject();
+            if (null == subject) {
+                throw new NullPointerException("No Subject in JAASUserPrincipal for user: "+
+                        principal.getName());
+            }
+            
+            Iterator keyLists = subject.getPrivateCredentials(UserKeys.class).iterator();
+            while (keyLists.hasNext()) {
+                keys.addAll(((UserKeys) keyLists.next()).getKeys());
+            }   
+        }
+
+        return keys;
+    }
+
+}
