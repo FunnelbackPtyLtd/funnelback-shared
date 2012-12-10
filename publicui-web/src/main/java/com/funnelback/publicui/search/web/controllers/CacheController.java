cb0cee3fe15635e0427ceb1165f6a96700b19bdc
Nicolas Guillaumin <nguillaumin@funnelback.com>
2012-12-10 02:52:42 +0000

    [FUN-4679] Some work on replacing cache.cgi with a Java equivalent
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@33760 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/CacheController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/CacheController.java
@@ -1,11 +1,16 @@
 package com.funnelback.publicui.search.web.controllers;
 
+import groovy.xml.XmlUtil;
+
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.log4j.Log4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +18,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.funnelback.common.Xml;
 import com.funnelback.common.config.Config;
 import com.funnelback.common.config.DefaultValues;
 import com.funnelback.common.config.Keys;
+import com.funnelback.common.io.store.RawBytesRecord;
+import com.funnelback.common.io.store.Record;
+import com.funnelback.common.io.store.Store;
+import com.funnelback.common.io.store.XmlRecord;
+import com.funnelback.common.io.store.Store.View;
+import com.funnelback.common.io.store.StoreType;
 import com.funnelback.publicui.search.model.collection.Collection;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.service.ConfigRepository;
@@ -25,6 +37,8 @@ import com.funnelback.publicui.search.web.binding.CollectionEditor;
 /**
  * Deal with cached copies
  */
+@Controller
+@Log4j
 public class CacheController {
 
 	@Autowired
@@ -42,25 +56,42 @@ public class CacheController {
 	public ModelAndView cache(HttpServletRequest request,
 			HttpServletResponse response,
 			@RequestParam(RequestParameters.COLLECTION) Collection collection,
-			String doc,
-			String url) throws Exception {
+			@RequestParam String url) throws Exception {
 		
-		if (cachedCopiesDisabled(collection.getConfiguration()) || doc == null) {
+		if (cachedCopiesDisabled(collection.getConfiguration()) || url == null) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			
 			return new ModelAndView(DefaultValues.FOLDER_WEB+"/"
 					+DefaultValues.FOLDER_TEMPLATES+"/"
 					+DefaultValues.FOLDER_MODERNUI+"/cached-copy-unavailable",
 					new HashMap<String, Object>());
-		} else {		
-			String content = dataRepository.getCachedDocument(collection, doc);
-			if (content != null) {
-				// TODO For now response as text/plain
-				response.setContentType("text/plain");
-				response.getWriter().write(content);
-			} else {
-				// Cached copy unavailable
-				response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		} else {
+			Store<? extends Record> store = StoreType.getStore(collection.getConfiguration(), View.live);
+			try {
+				store.open();
+				Record r = store.get(url);
+				
+				if (r != null) {
+					if (r instanceof RawBytesRecord) {
+						RawBytesRecord bytesRecord = (RawBytesRecord) r;
+						response.getOutputStream().write(bytesRecord.getContent());
+						return null;
+					} else if (r instanceof XmlRecord) {
+						XmlRecord xmlRecord = (XmlRecord) r;
+						response.getWriter().write(Xml.toString(xmlRecord.getContent()));
+						return null;
+					} else {
+						throw new UnsupportedOperationException("Unknown record type '"+r.getClass()+"'");
+					}
+					
+				} else {
+					// Record not found
+					response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+					return null;
+				}
+			} finally {
+				store.close();
 			}
-			return null;
 		}
 	}
 	
