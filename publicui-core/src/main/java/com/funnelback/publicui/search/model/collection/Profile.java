f6c4663c323aee8ba0046a9de93efd13e4f26c9c
Nicolas Guillaumin <nguillaumin@funnelback.com>
2012-12-12 05:10:26 +0000

    Imports cleanup
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@33820 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/collection/Profile.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/collection/Profile.java
@@ -2,7 +2,6 @@ package com.funnelback.publicui.search.model.collection;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /**
