a651f24a02537724999f57d4673d501a1880f0fa
Nicolas Guillaumin <nguillaumin@funnelback.com>
2012-12-19 03:30:26 +0000

    [FUN-5267] Added unit test for local and meta collections (cached copies not supported)
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@33897 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/web/controllers/cache/LocalCollectionCacheControllerTest.java
@@ -0,0 +1,58 @@
+package com.funnelback.publicui.test.search.web.controllers.cache;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.funnelback.common.config.DefaultValues;
+import com.funnelback.publicui.search.service.ConfigRepository;
+import com.funnelback.publicui.search.service.DataRepository;
+import com.funnelback.publicui.search.web.controllers.CacheController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/test/resources/spring/applicationContext.xml")
+public class LocalCollectionCacheControllerTest {
+
+	protected CacheController cacheController;
+	
+	@Resource(name="localConfigRepository")
+	protected ConfigRepository configRepository;
+	
+	@Resource(name="localDataRepository")
+	protected DataRepository dataRepository;
+	
+	protected MockHttpServletRequest request;
+	protected MockHttpServletResponse response;
+
+	@Before
+	public void before() throws IOException {
+		cacheController = new CacheController();
+		cacheController.setConfigRepository(configRepository);
+		cacheController.setDataRepository(dataRepository);
+		
+		request = new MockHttpServletRequest();
+		request.setRequestURI("/s/cache.html");
+		response = new MockHttpServletResponse();
+	}
+	
+	@Test
+	public void test() throws Exception {
+		cacheController.cache(request,
+				response,
+				configRepository.getCollection("cache-local"),
+				DefaultValues.PREVIEW_SUFFIX,
+				DefaultValues.DEFAULT_FORM,
+				"unknown-record");
+
+	}
+}
