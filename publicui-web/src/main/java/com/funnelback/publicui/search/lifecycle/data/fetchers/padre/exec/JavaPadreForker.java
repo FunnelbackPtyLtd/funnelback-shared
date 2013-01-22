0e3951e563d5f4d988b71cb8870b88cd6fe1f69f
Matthew Sheppard <msheppard@funnelback.com>
2013-01-22 06:54:00 +0000

    [SUPPORT-938] Add a watchdog to the JavaPadreForker to try to clean up if an out of memory exeption occurs in the PumpStreamHandler (otherwise the Java thread sits around waiting for padre to finish, and padre is blocked because nothing is reading from its output stream).
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@34067 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/data/fetchers/padre/exec/JavaPadreForker.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/data/fetchers/padre/exec/JavaPadreForker.java
@@ -5,10 +5,14 @@ import java.io.IOException;
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.ExecuteStreamHandler;
+import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.funnelback.publicui.i18n.I18n;
 
@@ -21,6 +25,10 @@ public class JavaPadreForker implements PadreForker {
 
 	private final I18n i18n;
 	
+    @Value("#{appProperties['padre.fork.java.timeout']?:60000}")
+	@Setter
+	protected long padreWaitTimeout;
+	
 	@Override
 	public PadreExecutionReturn execute(String commandLine, Map<String, String> environmnent) throws PadreForkingException {
 		
@@ -32,7 +40,13 @@ public class JavaPadreForker implements PadreForker {
 		log.debug("Executing '" + padreCmdLine + "' with environment " + environmnent);
 		
 		PadreExecutor executor = new PadreExecutor();
-		executor.setStreamHandler(new PumpStreamHandler(padreOutput, padreError, null));
+
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(padreWaitTimeout);
+		executor.setWatchdog(watchdog);
+
+		PumpStreamHandler streamHandler = new PumpStreamHandler(padreOutput, padreError, null);
+		
+		executor.setStreamHandler(streamHandler);
 		
 		try {
 			int rc = executor.execute(padreCmdLine, environmnent);
