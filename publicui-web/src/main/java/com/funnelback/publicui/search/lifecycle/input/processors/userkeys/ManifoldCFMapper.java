0af6dd8bbe00f38fef9371533716d1d8a1469fc7
Matthew Sheppard <msheppard@funnelback.com>
2013-04-15 05:20:15 +0000

    A stub ManifoldCF Mapper to user to get keys from the authority connector.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@34886 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/ManifoldCFMapper.java
@@ -0,0 +1,64 @@
+package com.funnelback.publicui.search.lifecycle.input.processors.userkeys;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+import com.funnelback.common.config.Keys;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+
+/**
+ * <p>Pass user keys based on a "userkeys" request parameter, which would normally be added
+ * by some external system which is wrapping the search results (a portal).</p>
+ * 
+ * <p>Note that this approach is not secure unless Funnelback can be accessed only via
+ * the portal.</p>
+ */
+public class ManifoldCFMapper implements UserKeysMapper {
+
+    /** Name of the URL parameter to use to pass the username 
+     * (TODO - Remove this and use the remote user, it's just for testing
+     * right now.) */
+    public static final String USERNAME_PARAMETER_NAME = "user";
+    
+    @Override
+    public List<String> getUserKeys(SearchTransaction transaction) {
+        try {
+            String username = transaction.getQuestion().getInputParameterMap().get(USERNAME_PARAMETER_NAME);
+            // TODO - Get the remote user instead somehow
+            
+            InputStream in = new URL(Keys.ManifoldCF.AUTHORITY_URL_PREFIX + "/UserACLs?username=" + username).openStream();
+            
+            String authorityInfo;
+            try {
+                authorityInfo = IOUtils.toString(in);
+            } finally {
+                IOUtils.closeQuietly(in);
+            }
+            
+            // TODO - remove after debugging
+            System.err.println(authorityInfo);
+            
+            List<String> result = new ArrayList<String>();
+            if (authorityInfo != null) {
+                for (String key: authorityInfo.split("\n")) {
+                    result.addAll(Arrays.asList(key.split(",")));
+                }
+            }
+            
+            return result;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
