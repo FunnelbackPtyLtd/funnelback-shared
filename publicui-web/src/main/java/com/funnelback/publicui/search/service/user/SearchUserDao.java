fb8ed2f920bcd2e55188b7951af3399bc629c1cf
Nicolas Guillaumin <nguillaumin@funnelback.com>
2013-04-29 07:07:28 +0000

    [FUN-5436] Fix wrong import
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@35071 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/service/user/SearchUserDao.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/user/SearchUserDao.java
@@ -5,7 +5,6 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.sqlite.JDBC;
 
 import com.funnelback.publicui.search.model.transaction.session.SearchUser;
 import com.funnelback.publicui.search.service.SearchUserRepository;
