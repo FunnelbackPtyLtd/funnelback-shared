a3bcffdf9e7897866b1a66e14269d051c830cfbb
Matthew Sheppard <msheppard@funnelback.com>
2013-05-06 05:15:59 +0000

    Provide authentication details for the service user (based on the server secret) when accessing the ManifoldCF authority.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@35144 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/ManifoldCFMapper.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/ManifoldCFMapper.java
@@ -2,6 +2,7 @@ package com.funnelback.publicui.search.lifecycle.input.processors.userkeys;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyManagementException;
@@ -15,8 +16,12 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 
+import com.funnelback.common.Security;
+import com.funnelback.common.Security.System;
+import com.funnelback.common.config.Config;
 import com.funnelback.common.config.Keys;
 import com.funnelback.common.net.TrustAllCertsX509TrustManager;
 import com.funnelback.publicui.search.lifecycle.input.InputProcessorException;
@@ -37,6 +42,8 @@ public class ManifoldCFMapper implements UserKeysMapper {
      * right now.) */
     public static final String USERNAME_PARAMETER_NAME = "user";
 
+    private final static String SERVICE_USERNAME = "_svc_manifoldcf";
+    
     public ManifoldCFMapper() throws NoSuchAlgorithmException, KeyManagementException {
         // Arrange to trust all SSL certificates (because the admin UI usually has a self signed one
         SSLContext sc = SSLContext.getInstance("SSL");
@@ -60,19 +67,26 @@ public class ManifoldCFMapper implements UserKeysMapper {
             String username = transaction.getQuestion().getInputParameterMap().get(USERNAME_PARAMETER_NAME);
             // TODO - Get the remote user instead somehow
             
-            String domain = transaction.getQuestion().getCollection().getConfiguration().value(Keys.ManifoldCF.DOMAIN);
+            Config config = transaction.getQuestion().getCollection().getConfiguration();
+            
+            String domain = config.value(Keys.ManifoldCF.DOMAIN);
 
-            String authority = transaction.getQuestion().getCollection().getConfiguration().value(Keys.ManifoldCF.AUTHORITY_URL_PREFIX);
+            String authority = config.value(Keys.ManifoldCF.AUTHORITY_URL_PREFIX);
             URL authorityUrl = new URL(authority + "/UserACLs?username=" + username + "@" + domain);
             
-            InputStream in;
-            if (authority.startsWith("https://")) {
-                HttpsURLConnection connection = (HttpsURLConnection) authorityUrl.openConnection();
-                connection.setHostnameVerifier(allHostsValidVerifier);
-                in = connection.getInputStream();
-            } else {
-                in = authorityUrl.openStream();
+            HttpURLConnection connection = (HttpURLConnection) authorityUrl.openConnection();
+            
+            if (connection instanceof HttpsURLConnection) {
+                // Ignore invalid HTTPS certificates
+                ((HttpsURLConnection) connection).setHostnameVerifier(allHostsValidVerifier);
             }
+            
+            String servicePassword = Security.generateSystemPassword(System.MANIFOLDCF, config.value(Keys.SERVER_SECRET));
+            String usernameAndPassword = SERVICE_USERNAME + ":" + servicePassword;
+            String basicAuth = "Basic " + new String(new Base64().encode(usernameAndPassword.getBytes()));
+            connection.setRequestProperty("Authorization", basicAuth);
+            
+            InputStream in = connection.getInputStream();
             
             String authorityInfo;
             try {
