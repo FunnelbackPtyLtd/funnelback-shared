e12fd46105c36e343e836603bc984e23fc77b177
Nicolas Guillaumin <nguillaumin@funnelback.com>
2013-05-24 04:10:35 +0000

    [FUN-5563] Fix XSLT test due to new <collapsed> tags
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@35339 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/test/java/com/funnelback/publicui/test/search/web/views/XSLTXStreamViewTests.java
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/web/views/XSLTXStreamViewTests.java
@@ -1,12 +1,13 @@
 package com.funnelback.publicui.test.search.web.views;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import com.funnelback.publicui.search.model.padre.ResultPacket;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.model.transaction.SearchResponse;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import com.funnelback.publicui.search.web.controllers.SearchController;
+import com.funnelback.publicui.search.web.views.XSLTXStreamView;
+import com.funnelback.publicui.xml.SearchXStreamMarshaller;
+import com.funnelback.publicui.xml.padre.StaxStreamParser;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
@@ -16,14 +17,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import com.funnelback.publicui.search.model.padre.ResultPacket;
-import com.funnelback.publicui.search.model.transaction.SearchQuestion;
-import com.funnelback.publicui.search.model.transaction.SearchResponse;
-import com.funnelback.publicui.search.model.transaction.SearchTransaction;
-import com.funnelback.publicui.search.web.controllers.SearchController;
-import com.funnelback.publicui.search.web.views.XSLTXStreamView;
-import com.funnelback.publicui.xml.SearchXStreamMarshaller;
-import com.funnelback.publicui.xml.padre.StaxStreamParser;
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class XSLTXStreamViewTests {
 
@@ -64,8 +63,8 @@ public class XSLTXStreamViewTests {
         oldXml = p.matcher(oldXml).replaceAll("");
         
         // line ends
-        oldXml = oldXml.replaceAll("\\r?\\n", "\n");        
-        actual = actual.replaceAll("\\r?\\n", "\n");
+        oldXml = oldXml.replaceAll("\r?\n", "\n");
+        actual = actual.replaceAll("\r?\n", "\n");
         p = Pattern.compile("\\s+$", Pattern.MULTILINE);
         oldXml = p.matcher(oldXml).replaceAll("");
         
@@ -77,35 +76,35 @@ public class XSLTXStreamViewTests {
         actual = actual.replaceAll("<\\?.*\\?>", "");
         
         // Trailing newline
-        oldXml = oldXml.replaceAll("\\n$", "");
-        actual = actual.replaceAll("\\n$", "");
+        oldXml = oldXml.replaceAll("\n$", "");
+        actual = actual.replaceAll("\n$", "");
         
         // Comments
-        oldXml = oldXml.replaceAll("<!--.*?-->\\n?", "");
+        oldXml = oldXml.replaceAll("<!--.*?-->\n?", "");
         
         // Newline between quicklinks tags
-        actual = actual.replaceAll("\\n?<(/)?quicklink>\\n?", "<$1quicklink>");
-        actual = actual.replaceAll("\\n?<(/)?qltext>\\n?", "<$1qltext>");
-        actual = actual.replaceAll("\\n?<(/)?qlurl>\\n?", "<$1qlurl>");
-        
-        // Newline between some results
-        p = Pattern.compile("(</result>).*?(<result>)", Pattern.DOTALL);
-        oldXml = p.matcher(oldXml).replaceAll("$1\n$2");
+        actual = actual.replaceAll("\n?<(/)?quicklink>\n?", "<$1quicklink>");
+        actual = actual.replaceAll("\n?<(/)?qltext>\n?", "<$1qltext>");
+        actual = actual.replaceAll("\n?<(/)?qlurl>\n?", "<$1qlurl>");
         
         // PADRE date contains space
-        p = Pattern.compile("<collection_updated>[.\\n]*(.*?)[\\s]*</collection_updated>", Pattern.DOTALL);
+        p = Pattern.compile("<collection_updated>[.\n]*(.*?)[\\s]*</collection_updated>", Pattern.DOTALL);
         oldXml = p.matcher(oldXml).replaceAll("<collection_updated>$1</collection_updated>");
 
         // Tier bars not supported in transformed XML
         p = Pattern.compile("<tier_bar>.*?</tier_bar>", Pattern.DOTALL);
         oldXml = p.matcher(oldXml).replaceAll("");
-        
+
+        // Newline between some results
+        p = Pattern.compile("(</result>)\\s+?(<result>)", Pattern.DOTALL);
+        oldXml = p.matcher(oldXml).replaceAll("$1\n$2");
+
         // Tags not supported in transformed XML
-        p = Pattern.compile("<tags>.*?</tags>\\r?\\n", Pattern.DOTALL);
+        p = Pattern.compile("<tags>.*?</tags>\r?\n", Pattern.DOTALL);
         oldXml = p.matcher(oldXml).replaceAll("");        
         
         // No click tracking URL
-        actual = actual.replaceAll("<click_tracking_url/>\\n", "");
+        actual = actual.replaceAll("<click_tracking_url/>\n", "");
         
         // Re-order <rmc> equally
         oldXml = reOrder(oldXml, Pattern.compile("<rmc.*</rmc>", Pattern.DOTALL));
@@ -118,44 +117,46 @@ public class XSLTXStreamViewTests {
         
         // <md f... needs to be tested separately because there is no easy way
         // to reorder them
-        p = Pattern.compile("<md f[^\\n]*</md>");
+        p = Pattern.compile("<md f[^\n]*</md>");
         Matcher m = p.matcher(oldXml);
         while (m.find()) {
             Assert.assertTrue(actual.contains(m.group(0)));
         }
         // Then strip the <md> tags
-        oldXml = oldXml.replaceAll("<md f[^\\n]*</md>", "");
-        actual = actual.replaceAll("<md f[^\\n]*</md>", "");
+        oldXml = oldXml.replaceAll("<md f[^\n]*</md>", "");
+        actual = actual.replaceAll("<md f[^\n]*</md>", "");
         
         // <explain> tags didn't exist prior to v11
-        oldXml = oldXml.replaceAll("(?s)<explain>.*?</explain>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<explain>.*?</explain>\n", "");
         // As well as <explain_types>
-        oldXml = oldXml.replaceAll("(?s)<explain_types>.*?</explain_types>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<explain_types>.*?</explain_types>\n", "");
         // As well as <cooler_weightings>
-        oldXml = oldXml.replaceAll("(?s)<cooler_weightings>.*?</cooler_weightings>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<cooler_weightings>.*?</cooler_weightings>\n", "");
         // As well as <cooler_names>
-        oldXml = oldXml.replaceAll("(?s)<cooler_names>.*?</cooler_names>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<cooler_names>.*?</cooler_names>\n", "");
         // As well as <stem_equivs>
-        oldXml = oldXml.replaceAll("(?s)<stem_equivs>.*?</stem_equivs>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<stem_equivs>.*?</stem_equivs>\n", "");
         // As well as <stop_words>
-        oldXml = oldXml.replaceAll("(?s)<stop_words>.*?</stop_words>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<stop_words>.*?</stop_words>\n", "");
         // As well as <qsup> for query blending
-        oldXml = oldXml.replaceAll("(?s)<qsup.*?>.*?</qsup>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<qsup.*?>.*?</qsup>\n", "");
         // As well as <km_from_origin> for geo-related features
-        oldXml = oldXml.replaceAll("(?s)<km_from_origin>.*?</km_from_origin>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<km_from_origin>.*?</km_from_origin>\n", "");
         // As well as <entitylist />
-        oldXml = oldXml.replaceAll("(?s)<entitylist>.*?</entitylist>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<entitylist>.*?</entitylist>\n", "");
         // As well as <svgs />
-        oldXml = oldXml.replaceAll("(?s)<svgs>.*?</svgs>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<svgs>.*?</svgs>\n", "");
         // As well as <datecount>
-        oldXml = oldXml.replaceAll("(?s)<datecount.*?>.*?</datecount>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<datecount.*?>.*?</datecount>\n", "");
         // As well as <collapsed>
-        oldXml = oldXml.replaceAll("(?s)<collapsed.*?>.*?</collapsed>\\n", "");
+        oldXml = oldXml.replaceAll("(?s)<collapsed.*?>.*?</collapsed>\n", "");
         
         // Remove <unexpected_tag> used for other tests
-        oldXml = oldXml.replaceAll("(?s)<unexpected_tag>.*?</unexpected_tag>\\n", "");
-        
-        
+        oldXml = oldXml.replaceAll("(?s)<unexpected_tag>.*?</unexpected_tag>\n", "");
+
+        // Ensure we're not comparing empty or very small strings
+        Assert.assertTrue(oldXml.length() > 50000);
+        Assert.assertTrue(actual.length() > 50000);
         Assert.assertEquals(oldXml, actual);
     }
     
@@ -168,7 +169,7 @@ public class XSLTXStreamViewTests {
     private String reOrder(String input, Pattern p) {
         Matcher m = p.matcher(input);
         if (m.find()) {
-            String rmcs[] = m.group(0).split("\\n");
+            String rmcs[] = m.group(0).split("\n");
             Arrays.sort(rmcs);
             return m.replaceAll(StringUtils.join(rmcs, "\n"));
         }
