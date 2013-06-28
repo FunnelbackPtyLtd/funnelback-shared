a5b479b42186cac3f0b1061dde9bfe7a3c155356
Luke Butters <lbutters@funnelback.com>
2013-06-28 06:59:58 +0000

    [FUN-5471] Unit tests and mock object for com.funnelback.publicui.search.lifecycle.input.processors.GeolocationTests.java
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@35598 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/test/java/com/funnelback/publicui/test/mock/MockGeolocator.java
@@ -0,0 +1,22 @@
+package com.funnelback.publicui.test.mock;
+
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.service.location.Geolocator;
+import com.maxmind.geoip.Location;
+
+
+public class MockGeolocator implements Geolocator{
+
+	public Location location;
+	
+	public MockGeolocator(){
+		this.location = new Location();
+		this.location.latitude = (float) 20;
+		this.location.longitude = (float) 100;
+	}
+	@Override
+	public Location geolocate(SearchQuestion searchQuestion) {
+		return this.location;
+	}
+
+}
---
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/lifecycle/input/processors/GeolocationTests.java
@@ -0,0 +1,180 @@
+package com.funnelback.publicui.test.search.lifecycle.input.processors;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.funnelback.common.EnvironmentVariableException;
+import com.funnelback.common.config.Config;
+import com.funnelback.common.config.Keys;
+import com.funnelback.common.config.NoOptionsConfig;
+import com.funnelback.publicui.search.lifecycle.input.InputProcessorException;
+import com.funnelback.publicui.search.lifecycle.input.processors.Geolocation;
+import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import com.funnelback.publicui.test.mock.MockGeolocator;
+
+
+public class GeolocationTests {
+
+	private Geolocation processor;
+	
+	private Config getDefaultConfig() throws FileNotFoundException {
+		return new NoOptionsConfig(new File("src/test/resources/dummy-search_home"), "dummy");
+	}
+	
+	private SearchTransaction getProcessedTransaction(Config conf) throws InputProcessorException {
+		SearchQuestion question = new SearchQuestion();
+		question.setCollection(new Collection("dummy", conf));
+		
+		SearchTransaction st = new SearchTransaction(question, null);
+		processor.processInput(st);
+		return st;
+	}
+	
+    @Before
+    public void before() {
+    	processor = new Geolocation();
+    	processor.setGeolocator(new MockGeolocator());
+    }
+	
+	@Test
+	public void testMissing() throws InputProcessorException, EnvironmentVariableException, FileNotFoundException {
+		//No transaction
+		this.processor.processInput(null);
+		
+		//no question
+		this.processor.processInput(new SearchTransaction(null, null));
+		
+		//no collection
+		SearchQuestion question = new SearchQuestion();
+        SearchTransaction st = new SearchTransaction(question, null);
+        this.processor.processInput(st);
+        if (null != st.getQuestion().getAdditionalParameters())
+        	Assert.assertFalse(st.getQuestion().getAdditionalParameters().containsKey("origin"));
+        Assert.assertNull(st.getQuestion().getLocation());
+        
+        
+	}
+	
+	@Test
+	public void testGeolocationEnabledAndOriginSet() throws FileNotFoundException, InputProcessorException {
+		Config conf = getDefaultConfig();
+		conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "true");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "true");
+		
+		SearchTransaction st = getProcessedTransaction(conf);
+		
+		Assert.assertNotNull(st.getQuestion().getAdditionalParameters());
+		Assert.assertTrue(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+        Assert.assertNotNull(st.getQuestion().getLocation());
+        Assert.assertEquals((float) 20, st.getQuestion().getLocation().latitude, 0.0); 
+        Assert.assertEquals((float) 100, st.getQuestion().getLocation().longitude, 0.0); 
+        
+        //
+       
+	}
+	
+	/**
+	 * Check to see what happens to the CGI param if origin is already set
+	 * @throws InputProcessorException 
+	 * @throws FileNotFoundException 
+	 */
+	@Test
+	public void originAlreadySetAndOriginSet() throws InputProcessorException, FileNotFoundException {
+		Config conf = getDefaultConfig();
+		conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "true");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "true");
+		//Set origin
+        SearchQuestion question = new SearchQuestion();
+		question.setCollection(new Collection("dummy", conf));
+		
+		SearchTransaction st = new SearchTransaction(question, null);
+		st.getQuestion().getAdditionalParameters().put(RequestParameters.ORIGIN, null);
+		processor.processInput(st);
+		
+        Assert.assertNotNull(st.getQuestion().getAdditionalParameters());
+		Assert.assertTrue(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+		Assert.assertNull("The processor should have left this as null", st.getQuestion().getAdditionalParameters().get(RequestParameters.ORIGIN));
+        Assert.assertNotNull(st.getQuestion().getLocation());
+        Assert.assertEquals((float) 20, st.getQuestion().getLocation().latitude, 0.0); 
+        Assert.assertEquals((float) 100, st.getQuestion().getLocation().longitude, 0.0);
+	}
+	
+	/**
+	 * Check to see what happens to the CGI param if origin is already set
+	 * @throws InputProcessorException 
+	 * @throws FileNotFoundException 
+	 */
+	@Test
+	public void originAlreadySetAndOriginNotSet() throws InputProcessorException, FileNotFoundException {
+		Config conf = getDefaultConfig();
+		conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "true");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "false");
+		//Set origin
+        SearchQuestion question = new SearchQuestion();
+		question.setCollection(new Collection("dummy", conf));
+		
+		SearchTransaction st = new SearchTransaction(question, null);
+		st.getQuestion().getAdditionalParameters().put(RequestParameters.ORIGIN, null);
+		processor.processInput(st);
+		
+        Assert.assertNotNull(st.getQuestion().getAdditionalParameters());
+		Assert.assertTrue(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+		Assert.assertNull("The processor should have left this as null", st.getQuestion().getAdditionalParameters().get(RequestParameters.ORIGIN));
+        Assert.assertNotNull(st.getQuestion().getLocation());
+        Assert.assertEquals((float) 20, st.getQuestion().getLocation().latitude, 0.0); 
+        Assert.assertEquals((float) 100, st.getQuestion().getLocation().longitude, 0.0);
+	}
+	
+	
+	
+	@Test 
+	public void testGeolocationEnabled() throws FileNotFoundException, InputProcessorException{
+		Config conf = getDefaultConfig();
+		conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "true");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "false");
+        
+        SearchTransaction st = getProcessedTransaction(conf);
+		
+		if (null != st.getQuestion().getAdditionalParameters())
+			Assert.assertFalse(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+        Assert.assertNotNull(st.getQuestion().getLocation());
+        Assert.assertEquals((float) 20, st.getQuestion().getLocation().latitude, 0.0); 
+        Assert.assertEquals((float) 100, st.getQuestion().getLocation().longitude, 0.0); 
+        
+        //Ensure that orgin stays 
+	}
+	
+	@Test
+	public void testGeolocationDisabledAndOrginEnabled() throws FileNotFoundException, InputProcessorException{
+		Config conf = getDefaultConfig();
+		conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "false");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "true");
+        
+        SearchTransaction st = getProcessedTransaction(conf);
+        
+        if (null != st.getQuestion().getAdditionalParameters())
+        	Assert.assertFalse(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+        Assert.assertNull(st.getQuestion().getLocation());
+	}
+	
+	@Test
+	public void testBothDisbled() throws FileNotFoundException, InputProcessorException {
+		//Keys.ModernUI.GEOLOCATION_ENABLED == false && Keys.ModernUI.GEOLOCATION_SET_ORIGIN == false
+        Config conf = getDefaultConfig();
+        conf.setValue(Keys.ModernUI.GEOLOCATION_ENABLED, "false");
+        conf.setValue(Keys.ModernUI.GEOLOCATION_SET_ORIGIN, "false");
+        SearchTransaction st = getProcessedTransaction(conf);
+        
+        if (null != st.getQuestion().getAdditionalParameters())
+        	Assert.assertFalse(st.getQuestion().getAdditionalParameters().containsKey(RequestParameters.ORIGIN));
+        Assert.assertNull(st.getQuestion().getLocation());
+	}
+
+}
