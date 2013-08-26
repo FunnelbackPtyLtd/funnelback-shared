95bc5aec0675ee0ba33286ea40938513d64a5f03
Nicolas Guillaumin <nguillaumin@funnelback.com>
2013-08-26 23:13:39 +0000

    Added equals() and hashCode() as required by JPA/Hibernate for ID classes
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@36099 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/session/SessionResultPK.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/session/SessionResultPK.java
@@ -2,11 +2,14 @@ package com.funnelback.publicui.search.model.transaction.session;
 
 import java.io.Serializable;
 
+import lombok.EqualsAndHashCode;
+
 /**
  * Internal Primary Key class for {@link SessionResult}
  *
  * @since v12.5
  */
+@EqualsAndHashCode
 public class SessionResultPK implements Serializable {
 
     private static final long serialVersionUID = -694314277777986501L;
