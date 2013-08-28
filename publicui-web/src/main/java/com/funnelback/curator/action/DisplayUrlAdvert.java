35e6b8b461bbdc39192e24119125750658a949c8
Matthew Sheppard <msheppard@funnelback.com>
2013-08-28 06:01:19 +0000

    [CR-61] Allow the DisplayUrlAdvert action to be configured not to remove the organic result (in case people want duplicates for some reason).
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@36143 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/curator/action/DisplayUrlAdvert.java
+++ publicui-web/src/main/java/com/funnelback/curator/action/DisplayUrlAdvert.java
@@ -22,13 +22,27 @@ public class DisplayUrlAdvert implements Action {
     private UrlAdvert advert;
 
     /**
+     * <p>
+     * Whether to remove the displayURL of the UrlAdvert if it also exists
+     * within the organic results.
+     * </p>
+     * 
+     * <p>
+     * Defaults to true.
+     * </p>
+     */
+    @Getter
+    @Setter
+    private boolean removeCollectionUrl = true;
+
+    /**
      * Adds the action's URL advert object to the curator model's exhibits and
      * removes any result with the same display URL from the main search
      * results.
      */
     @Override
     public void performAction(SearchTransaction searchTransaction, Phase phase) {
-        if (phase.equals(Phase.INPUT)) {
+        if (removeCollectionUrl && phase.equals(Phase.INPUT)) {
             // Remove the URL if it exists in the results
             RemoveUrls.addUrlsStringToRemoveList(searchTransaction, advert.getDisplayUrl());
         } else if (phase.equals(Phase.OUTPUT)) {
@@ -43,6 +57,6 @@ public class DisplayUrlAdvert implements Action {
      */
     @Override
     public boolean runsInPhase(Phase phase) {
-        return phase.equals(Phase.INPUT) || phase.equals(Phase.OUTPUT);
+        return (removeCollectionUrl && phase.equals(Phase.INPUT)) || phase.equals(Phase.OUTPUT);
     }
 }
