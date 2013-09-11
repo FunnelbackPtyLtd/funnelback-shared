7a00f3f9732f20a8c3f26f8c1179056247597673
Luke Butters <lbutters@funnelback.com>
2013-09-11 07:02:20 +0000

    Removed the seperate recomender servlet. THis will enable us to autowire in the searchController. That can be used to run a query and use the existing lifecycle, from their we can take the returned model and use that to construct a recomender object for explorer.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@36372 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/recommender/web/controllers/RecommenderController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/recommender/web/controllers/RecommenderController.java
@@ -6,15 +6,28 @@ import com.funnelback.publicui.recommender.Recommendation;
 import com.funnelback.publicui.recommender.RecommendationResponse;
 import com.funnelback.publicui.recommender.SortType;
 import com.funnelback.publicui.recommender.utils.RecommenderUtils;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
+import com.funnelback.publicui.search.model.transaction.session.SearchUser;
+import com.funnelback.publicui.search.web.controllers.SearchController;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import java.util.*;
 
 /**
@@ -24,80 +37,106 @@ import java.util.*;
 @Controller
 @RequestMapping("/recommender")
 public class RecommenderController {
-  	private static FBRecommender fbRecommender;
+	private static FBRecommender fbRecommender;
 	public static final String searchRecommendationsHtml = "searchRecommendations.html";
 	public static final String queryEntryHtml = "queryEntry.html";
-    public static final String itemEntryHtml = "itemEntry.html";
-    public static final String similarItemsJson = "similarItems.json";
-    public static final String sessionsHtml = "sessions.html";
+	public static final String itemEntryHtml = "itemEntry.html";
+	public static final String similarItemsJson = "similarItems.json";
+	public static final String explorerJson = "explore.json";
+	public static final String sessionsHtml = "sessions.html";
 
 	static {
 		// Get a single instance of the Recommender.
-        fbRecommender = FBRecommender.getInstance();
+		fbRecommender = FBRecommender.getInstance();
 	}
+
+	public enum ModelAttributes {
+		SearchTransaction, AllCollections, QueryString, SearchPrefix, ContextPath, Log,
+		extraSearches, question, response, session, error, httpRequest;
+
+		public static Set<String> getNames() {
+			HashSet<String> out = new HashSet<>();
+			for (ModelAttributes name: values()) {
+				out.add(name.toString());
+			}
+			return out;
+		}
+	}
+
+	@Autowired
+	private LocaleResolver localeResolver;
 	
-	 public enum ModelAttributes {
-	        SearchTransaction, AllCollections, QueryString, SearchPrefix, ContextPath, Log,
-	        extraSearches, question, response, session, error, httpRequest;
-	        
-	        public static Set<String> getNames() {
-	            HashSet<String> out = new HashSet<>();
-	            for (ModelAttributes name: values()) {
-	                out.add(name.toString());
-	            }
-	            return out;
-	        }
-	 }
-	 
+	@Autowired
+    private SearchController searchController;
+	
+	@InitBinder
+    public void initBinder(DataBinder binder) {
+        searchController.initBinder(binder);
+    }
+
 	@Resource(name="jsonView")
 	private View view;
 
-    /**
+	/**
 	 * Return JSON output showing similar (recommended) items for the given item name.
 	 *
-     * @param seedItem name of seed item to get recommended items for
-     * @param collection collection ID
-     * @param scope comma separated list of scopes e.g. cmis.csiro.au,-vic.cmis.csiro.au
-     * @param dsort descending sort parameter (optional)
-     * @param asort ascending sort parameter (optional)
-     * @return String containing recommendations, in JSON format
+	 * @param seedItem name of seed item to get recommended items for
+	 * @param collection collection ID
+	 * @param scope comma separated list of scopes e.g. cmis.csiro.au,-vic.cmis.csiro.au
+	 * @param dsort descending sort parameter (optional)
+	 * @param asort ascending sort parameter (optional)
+	 * @return String containing recommendations, in JSON format
 	 * @throws Exception
 	 */
-    @RequestMapping(value={"/" + similarItemsJson}, method = RequestMethod.GET)
-    public ModelAndView similarItems(HttpServletResponse response,
-    		@RequestParam("seedItem") String seedItem,
-    		@RequestParam("collection") String collection,
-    		@RequestParam(value = "scope", required = false) String scope,
-            @RequestParam(value = "dsort", required = false) String dsort,
-            @RequestParam(value = "asort", required = false) String asort,
-            @RequestParam(value = "metadataClass", required = false) String metadataClass) throws Exception {
-    	response.setContentType("application/json");
-        Comparator<Recommendation> comparator;
+	@RequestMapping(value={"/" + similarItemsJson}, method = RequestMethod.GET)
+	public ModelAndView similarItems(HttpServletResponse response,
+			@RequestParam("seedItem") String seedItem,
+			@RequestParam("collection") String collection,
+			@RequestParam(value = "scope", required = false) String scope,
+			@RequestParam(value = "dsort", required = false) String dsort,
+			@RequestParam(value = "asort", required = false) String asort,
+			@RequestParam(value = "metadataClass", required = false) String metadataClass) throws Exception {
+		response.setContentType("application/json");
+		Comparator<Recommendation> comparator;
 
-        if (seedItem == null || ("").equals(seedItem)) {
-            throw new IllegalArgumentException("seedItem parameter must be provided.");
-        }
+		if (seedItem == null || ("").equals(seedItem)) {
+			throw new IllegalArgumentException("seedItem parameter must be provided.");
+		}
 
-        if (collection == null || ("").equals(collection)) {
-            throw new IllegalArgumentException("collection parameter must be provided.");
-        }
+		if (collection == null || ("").equals(collection)) {
+			throw new IllegalArgumentException("collection parameter must be provided.");
+		}
 
-        if (metadataClass != null || ("").equals(metadataClass)) {
-            if (!DocInfoQuery.isValidMetadataClass(metadataClass)) {
-                throw new IllegalArgumentException("metadataClass parameter value is invalid: " + metadataClass);
-            }
-        }
+		if (metadataClass != null || ("").equals(metadataClass)) {
+			if (!DocInfoQuery.isValidMetadataClass(metadataClass)) {
+				throw new IllegalArgumentException("metadataClass parameter value is invalid: " + metadataClass);
+			}
+		}
 
-        comparator = SortType.getComparator(asort, dsort, metadataClass);
+		comparator = SortType.getComparator(asort, dsort, metadataClass);
 
-        List<Recommendation> recommendations =
-                RecommenderUtils.getRecommendationsForItem(fbRecommender, seedItem, collection, scope, 5);
-        
-        RecommendationResponse recommendationResponse =
-                new RecommendationResponse(RecommenderUtils.sortRecommendations(recommendations, comparator));
-        Map<String, Object> model = new HashMap<>();
-        model.put("RecommendationResponse", recommendationResponse);
-         
- 		return new ModelAndView(view, model);
-    }
+		List<Recommendation> recommendations =
+				RecommenderUtils.getRecommendationsForItem(fbRecommender, seedItem, collection, scope, 5);
+
+		RecommendationResponse recommendationResponse =
+				new RecommendationResponse(RecommenderUtils.sortRecommendations(recommendations, comparator));
+		Map<String, Object> model = new HashMap<>();
+		model.put("RecommendationResponse", recommendationResponse);
+
+		return new ModelAndView(view, model);
+	}
+
+	@RequestMapping(value={"/" + explorerJson}, method = RequestMethod.GET, params={RequestParameters.COLLECTION})
+	public ModelAndView similarItems(HttpServletRequest request,
+			HttpServletResponse response ,
+			@Valid SearchQuestion question,
+			@ModelAttribute SearchUser user
+			) throws Exception {
+		response.setContentType("application/json");
+		
+		Map<String, Object> model = new HashMap<>();
+		//model.put("RecommendationResponse",question);
+
+		return new ModelAndView(view, model);
+	}
 }
