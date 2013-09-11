709191c6b821b4bdc7c4c0b8388ce10f992176ad
Luke Butters <lbutters@funnelback.com>
2013-09-11 01:01:16 +0000

    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@36361 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/service/index/DefaultQueryReadLock.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/index/DefaultQueryReadLock.java
@@ -23,14 +23,14 @@ public class DefaultQueryReadLock implements QueryReadLock{
 	@Override
 	public void lock(Collection collection) throws FileLockException {
 		DefaultQueryReadLockSingleton.getDefaultQueryReadLockSingleton().lock(collection);
-		
+
 	}
 
 	@Override
 	public void release(Collection collection) {
 		DefaultQueryReadLockSingleton.getDefaultQueryReadLockSingleton().release(collection);
 	}
-	
+
 	/**
 	 * This Singleton should be used to lock the indexUpdate lock file while padre is run.
 	 * 
@@ -42,52 +42,52 @@ public class DefaultQueryReadLock implements QueryReadLock{
 	public static class DefaultQueryReadLockSingleton {
 
 		@Autowired
-	    protected static I18n i18n;
+		protected static I18n i18n;
 		/*
 		 * This holds all of the locks. At most there is one lock per collection. This is initially empty
 		 * and locks must be added at most once per collection.
 		 */
 		private ConcurrentMap<String , ThreadSharedFileLock> lockMap = new ConcurrentHashMap<String , ThreadSharedFileLock>();
-		
+
 		private DefaultQueryReadLockSingleton() { }
-		
+
 		/*
 		 * Get the only instance of this class
 		 */
 		@Getter
 		private static final DefaultQueryReadLockSingleton defaultQueryReadLockSingleton = new DefaultQueryReadLockSingleton();
-		
+
 		private String getKey(Config config) {
 			return config.getCollectionName();
 		}
-		
+
 		private String getCollectionUpdateLockFile(Config config) {
 			return config.getCollectionRoot()
 					+ File.separator + DefaultValues.VIEW_LIVE 
 					+ File.separator + DefaultValues.FOLDER_IDX
 					+ File.separator + Files.Index.UPDATE_LOCK;
 		}
-		
-	    public void lock(Collection collection) throws FileLockException {
-	    	Config config = collection.getConfiguration();
-	    	String key = getKey(config);
-	    	
-	    	//Ensure that the lockMap knows about this file to lock on.
-	    	//Without this IF we would have to create a new instance of ThreadSharedFileLock even if it was already in the
-	    	//map on every single call. Keep in mind this is called for every query. 
-	    	if (null == lockMap.get(key)){
-	    		//We must be atomic, another thread might have completed the above IF at the same time or just before 
-	    		//us
-	    		lockMap.putIfAbsent(key, new ThreadSharedFileLock(getCollectionUpdateLockFile(config)));
-	    	}
-	    	
-	    	lockMap.get(key).lock();
-	    }
-	    
-	    public void release(Collection collection) {
-	    	Config config = collection.getConfiguration();
-	    	String key = getKey(config);
-	    	lockMap.get(key).release();
-	    }
+
+		public void lock(Collection collection) throws FileLockException {
+			Config config = collection.getConfiguration();
+			String key = getKey(config);
+
+			//Ensure that the lockMap knows about this file to lock on.
+			//Without this IF we would have to create a new instance of ThreadSharedFileLock even if it was already in the
+			//map on every single call. Keep in mind this is called for every query. 
+			if (null == lockMap.get(key)){
+				//We must be atomic, another thread might have completed the above IF at the same time or just before 
+				//us
+				lockMap.putIfAbsent(key, new ThreadSharedFileLock(getCollectionUpdateLockFile(config)));
+			}
+
+			lockMap.get(key).lock();
+		}
+
+		public void release(Collection collection) {
+			Config config = collection.getConfiguration();
+			String key = getKey(config);
+			lockMap.get(key).release();
+		}
 	}
 }
