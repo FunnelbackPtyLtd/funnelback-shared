2d754d7514c1dfe059d810ca45e0478e55ac8176
Matthew Sheppard <msheppard@funnelback.com>
2014-02-11 05:06:08 +0000

    [SUPPORT-1347] Make ehcache use a disk store which has a path set to include the execution context, meaning that the public and admin contexts will not clobber each other's cache.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@38108 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/service/AppCacheManager.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/AppCacheManager.java
@@ -1,33 +1,62 @@
 package com.funnelback.publicui.search.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.annotation.PreDestroy;
 
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.ConfigurationFactory;
+import net.sf.ehcache.config.DiskStoreConfiguration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import com.funnelback.publicui.utils.web.ExecutionContextHolder;
+
 /**
- * Bean used to properly shut down EHCache when Spring shuts
- * down. Helps when the app is run under Eclipse / Tomcat
+ * Bean used to construct and shutdown the EhCache instance
  */
 @Component
 @Log4j
 public class AppCacheManager {
     
     @Autowired
-    @Setter
-    private CacheManager appCacheManager;
+    private ExecutionContextHolder executionContextHolder;
+    
+    /**
+     * @return Returns a CacheManager configured for the current execution context (e.g. Public or Admin)
+     * to ensure that the caches for multiple deployments of the war do not clobber each other.
+     */
+    @Bean
+    public CacheManager cacheManager() throws CacheException, IOException {
+        try (InputStream cacheConfigInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("ehcache.xml")) {
+            Configuration configuration = ConfigurationFactory.parseConfiguration(cacheConfigInputStream);
+            
+            // Configure the diskStorage - Must be different for different execution contexts (SUPPORT-1347)
+            DiskStoreConfiguration diskStoreConfiguration = new DiskStoreConfiguration();
+            diskStoreConfiguration.setPath("java.io.tmpdir/EhCache-" + executionContextHolder.getExecutionContext().name());
+            configuration.addDiskStore(diskStoreConfiguration);
+    
+            return new CacheManager(configuration);
+        }
+    }
+
+    @Autowired
+    private CacheManager cacheManager;
 
     /**
-     * Shuts EHCache down
+     * Properly shut down EHCache when Spring shuts down.
+     * Helps when the app is run under Eclipse / Tomcat
      */
     @PreDestroy
     public void preDestroy() {
         log.debug("Shutting down application cache");
-        appCacheManager.shutdown();
+        cacheManager.shutdown();
     }
 
 }
