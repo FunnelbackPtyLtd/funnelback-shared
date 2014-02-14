45c9b67c589c43941dc2c4fe43985bcb4ab5c1f9
Matthew Sheppard <msheppard@funnelback.com>
2014-02-14 04:19:38 +0000

    Fixed unused import warnings.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@38155 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/ImageScaleController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/ImageScaleController.java
@@ -3,14 +3,12 @@ package com.funnelback.publicui.search.web.controllers;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import lombok.Cleanup;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
