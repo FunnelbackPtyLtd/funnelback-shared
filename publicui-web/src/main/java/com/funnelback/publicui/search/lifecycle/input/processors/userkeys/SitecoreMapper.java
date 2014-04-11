0449d5e773fb55d9133f4a21f671db33094f8448
John Haynes <jhaynes@funnelback.com>
2014-04-11 07:10:02 +0000

    [FUN-6475] - Added a bunch more logging and null checks
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@38706 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/SitecoreMapper.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/input/processors/userkeys/SitecoreMapper.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 
 import lombok.extern.log4j.Log4j;
+
 import com.funnelback.common.config.Keys;
 import com.funnelback.publicui.search.model.collection.Collection;
 import com.funnelback.publicui.search.model.transaction.SearchTransaction;
@@ -22,29 +24,46 @@ public class SitecoreMapper implements UserKeysMapper {
     @Override
     public List<String> getUserKeys(Collection currentCollection, SearchTransaction transaction) {
 
-        //Assemble the url to query
-        String serviceUrl =
-
-            //Pull the start of the URL from collection.cfg
-            //e.g. http://asc/sitecore/shell/userservice/userservice.asmx/UserGroups?username=
-            currentCollection.getConfiguration().value(Keys.SecurityEarlyBinding.SITECORE_SERVICE_URL)
-
-            //Attach the current search user to the end
-            + transaction.getQuestion().getPrincipal().getName();
-
-        System.out.println(currentCollection.getConfiguration().value(Keys.SecurityEarlyBinding.SITECORE_SERVICE_URL));
-        System.out.println(transaction.getQuestion().getPrincipal().getName());
-
         //Storage for keys to return
         List<String> userKeys = new ArrayList<String>();
 
+        String urlStem = null;
+        String searchName = null;
         try {
+
+            //Assemble the url to query
+            try {
+                //Pull the start of the URL from collection.cfg
+                //e.g. http://asc/sitecore/shell/userservice/userservice.asmx/UserGroups?username=
+                urlStem = currentCollection.getConfiguration().value(Keys.SecurityEarlyBinding.SITECORE_SERVICE_URL);
+                if(urlStem == null) {
+                    throw new Exception ("urlStem was null");
+                }
+            } catch (Exception e) {
+                log.error("Unable to get sitecore service url out of collection.cfg", e);
+                //Return immediately
+                return userKeys;
+            }
+
+            
+            try {
+                //Attach the current search user to the end
+                searchName = transaction.getQuestion().getPrincipal().getName();
+                if(searchName == null) {
+                    throw new Exception("searchName was null");
+                }
+            } catch (Exception e) {
+                log.error("Unable to get principle name from search transaction", e);
+                //Return immediately
+                return userKeys;
+            }
+
             //Parse the xml
             Document doc =
                 DocumentBuilderFactory
                     .newInstance()
                     .newDocumentBuilder()
-                    .parse(serviceUrl);
+                    .parse(urlStem + searchName);
 
             doc.normalize();
 
@@ -58,7 +77,7 @@ public class SitecoreMapper implements UserKeysMapper {
                 userKeys.add(s.trim());
             }
         } catch (Exception e) {
-            log.error("Hit " + e.toString() + " trying to parse xml from " + serviceUrl, e);
+            log.error("Hit " + e.toString() + " trying to parse xml from " + urlStem + " with searchName " + searchName, e);
         }
         return userKeys;
     }
