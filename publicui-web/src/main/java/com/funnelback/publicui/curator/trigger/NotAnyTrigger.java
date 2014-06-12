bf60823f37740dced8f502190144db477072f93a
Matthew Sheppard <msheppard@funnelback.com>
2014-06-12 23:56:16 +0000

    Apparently, for all my talk about 'not' curator triggers, such a thing was never implemented. It sounds useful to me, and the editor interface already has support for it, so I'm adding a trigger to implement it.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@39340 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/curator/trigger/NotAnyTrigger.java
@@ -0,0 +1,55 @@
+package com.funnelback.publicui.curator.trigger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import com.funnelback.publicui.search.model.curator.config.Trigger;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+
+/**
+ * <p>
+ * A trigger which activates only when none of the given 'sub' triggers activate.
+ * </p>
+ * 
+ * <p>
+ * This trigger can be used to invert other triggers, e.g. a trigger requiring
+ * that a date does not fall in a specified range.
+ * </p>
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class NotAnyTrigger implements Trigger {
+
+    /**
+     * The list of 'sub' triggers each of which must not activate for this trigger
+     * to activate.
+     */
+    @Getter
+    @Setter
+    private List<Trigger> triggers = new ArrayList<Trigger>();
+
+    /**
+     * Check each 'sub' trigger in turn to see if it activates on the given
+     * searchTransaction, and return true only if none of them activate (otherwise
+     * return false).
+     */
+    @Override
+    public boolean activatesOn(SearchTransaction searchTransaction, ApplicationContext context) {
+        for (Trigger trigger : triggers) {
+            if (trigger.activatesOn(searchTransaction, context)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
