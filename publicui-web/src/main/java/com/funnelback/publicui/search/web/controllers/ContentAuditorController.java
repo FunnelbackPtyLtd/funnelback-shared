e2cca529e953bd7aaa09c1092080a44533f917b1
Matthew Sheppard <msheppard@funnelback.com>
2014-10-29 05:35:00 +0000

    [FUN-7194] Initial work on automatic configuration of facets - Not yet sure how to deal with ones which are currently done with gscopes (unless we just start setting those gscopes on every collection automatically).
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@40938 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/ContentAuditorController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/ContentAuditorController.java
@@ -1,5 +1,8 @@
 package com.funnelback.publicui.search.web.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +18,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.funnelback.common.config.Config;
 import com.funnelback.common.config.DefaultValues;
+import com.funnelback.common.config.Keys;
+import com.funnelback.publicui.search.model.collection.FacetedNavigationConfig;
+import com.funnelback.publicui.search.model.collection.facetednavigation.CategoryDefinition;
+import com.funnelback.publicui.search.model.collection.facetednavigation.FacetDefinition;
+import com.funnelback.publicui.search.model.collection.facetednavigation.impl.DateFieldFill;
+import com.funnelback.publicui.search.model.collection.facetednavigation.impl.MetadataFieldFill;
+import com.funnelback.publicui.search.model.collection.facetednavigation.impl.URLFill;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.model.transaction.session.SearchUser;
@@ -60,7 +71,11 @@ public class ContentAuditorController {
 
         System.out.println(request.getRequestURL() + "?" + request.getQueryString());
         
-        question.getCollection().getConfiguration().setValue("ui.modern.search_link", "content-auditor.html");
+        // We want search result links to come back to us
+        // But still allow config to make it absolute
+        String searchLink = question.getCollection().getConfiguration().value(Keys.ModernUI.SEARCH_LINK);
+        searchLink = searchLink.replace("search.html", "content-auditor.html");
+        question.getCollection().getConfiguration().setValue(Keys.ModernUI.SEARCH_LINK, searchLink);
         
         // TODO - Manipulate the request if we need to
         question.getRawInputParameters().put(RequestParameters.NUM_RANKS, new String[] {"999"});
@@ -69,7 +84,46 @@ public class ContentAuditorController {
             question.setQuery("-padrenullquery");
         }
         
-        // TODO - Manipulate the facet config (somehow)
+        // TODO - Source this form somewhere? A collection.cfg setting?
+        Map<String, Character> metadataFields = new HashMap<String, Character>();
+        metadataFields.put("Creator", 'a');
+        metadataFields.put("Publisher", 'p');
+        metadataFields.put("Subject", 's');
+        metadataFields.put("Format", 'f');
+        metadataFields.put("Generator", 'y');
+        metadataFields.put("Page Type", '0');
+        metadataFields.put("Business Stage", 'B');
+        metadataFields.put("Business Structure", 'C');
+        metadataFields.put("Four Pillars", 'D');
+        metadataFields.put("Red Tape Reduction", 'E');
+
+        // Overwrite the facet config with a custom one
+        String qpOptions = " -rmcf="+constructRmcfValue(metadataFields)+" -count_dates=d -count_urls=1000";
+        List<FacetDefinition> facetDefinitions = new ArrayList<FacetDefinition>();
+        
+        // TODO - Source the base URL from somewhere (a collection.cfg setting?)
+        facetDefinitions.add(createPathFacetDefinition("Path", "http://www.business.qld.gov.au/"));
+        facetDefinitions.add(createDateFacetDefinition("Date modified"));
+        
+        for (Map.Entry<String, Character> entry : metadataFields.entrySet()) {
+            facetDefinitions.add(createMetadataFacetDefinition(entry.getKey(), entry.getValue()));            
+        }
+
+        /*
+         * TODO - We need to somehow support detecting missing metadata, ideally without having
+         * to set gscope bits or running extra queries.
+         * 
+         * The current implementation uses gscope bits set with something like...
+         * 
+         *     <QueryItem>
+         *       <Data>Subject</Data>
+         *       <Query>-s:$++ $++</Query>
+         *     </QueryItem>
+         */
+        
+        FacetedNavigationConfig facetedNavigationConfig = new FacetedNavigationConfig(qpOptions, facetDefinitions);
+        
+        question.getCollection().setFacetedNavigationLiveConfig(facetedNavigationConfig);
         
         ModelAndView mav =
             searchController.search(request, response, question, user);
@@ -90,6 +144,54 @@ public class ContentAuditorController {
             type;
         
         return new ModelAndView(viewName, model);
+    }
+
+    /**
+     * Return an rmcf query processor option value covering all the given metadata fields
+     */
+    private String constructRmcfValue(Map<String, Character> metadataFields) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Character> entry : metadataFields.entrySet()) {
+            result.append(entry.getValue());
+        }
+        
+        return result.toString();
+    }
+
+    /**
+     * Creates a date based facet definition with the given label
+     */
+    private FacetDefinition createDateFacetDefinition(String label) {
+        List<CategoryDefinition> categoryDefinitions = new ArrayList<CategoryDefinition>();
+        DateFieldFill fill = new DateFieldFill();
+        fill.setData("d");
+        categoryDefinitions.add(fill);
+        
+        return new FacetDefinition(label, categoryDefinitions);
+    }
+
+    /**
+     * Creates a URL path based facet definition with the given label, starting from urlBase
+     */
+    private FacetDefinition createPathFacetDefinition(String label, String urlBase) {
+        List<CategoryDefinition> categoryDefinitions = new ArrayList<CategoryDefinition>();
+        URLFill fill = new URLFill();
+        fill.setData(urlBase);
+        categoryDefinitions.add(fill);
+        
+        return new FacetDefinition(label, categoryDefinitions);
+    }
+
+    /**
+     * Creates a metadata field based facet definition with the given label, populated from the given metadataClass
+     */
+    private FacetDefinition createMetadataFacetDefinition(String Label, Character character) {
+        List<CategoryDefinition> categoryDefinitions = new ArrayList<CategoryDefinition>();
+        MetadataFieldFill fill = new MetadataFieldFill();
+        fill.setData(character.toString());
+        categoryDefinitions.add(fill);
+        
+        return new FacetDefinition(Label, categoryDefinitions);
     }
 
 }
