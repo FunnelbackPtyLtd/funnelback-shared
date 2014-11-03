03db4724f2ab68aeb7df66a19a54577a7c4f0884
Luke Butters <lbutters@funnelback.com>
2014-11-03 03:46:58 +0000

    FUN-7112 1) Included crawler as a dependency. 2) Added a class which is able to use similar (not the same) rules for checking if a URL matches the crawler include/exclude patterns. To use the exact some rules the crawler uses is too hard.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@40968 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-core/src/main/java/com/funnelback/publicui/utils/UrlStatus.java
@@ -0,0 +1,64 @@
+package com.funnelback.publicui.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import lombok.extern.log4j.Log4j;
+
+import com.funnelback.common.config.Collection;
+import com.funnelback.common.config.Config;
+import com.funnelback.common.config.Files;
+import com.funnelback.common.config.Keys;
+import com.funnelback.common.config.NoOptionsConfig;
+import com.funnelback.common.policy.SimpleLoadingPolicy;
+import com.funnelback.crawler.config.CrawlerConfig;
+
+@Log4j
+public class UrlStatus {
+
+    public static enum MatchResult {
+        MATCHES,
+        FAILS,
+        NOT_RELEVENT;
+    }
+    
+    public static MatchResult UrlMatchesCrawlerIncludeExcludePattern(File searchHome, String collection, String url) {
+        Config config;
+        try {
+            config = new NoOptionsConfig(searchHome, collection);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        if(Collection.Type.push2.toString().equals(config.value(Keys.COLLECTION_TYPE))){
+            return MatchResult.NOT_RELEVENT;
+        }
+        SimpleLoadingPolicy loadingPolicy = new SimpleLoadingPolicy();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PrintWriter printWriter = new PrintWriter(bos);//will use default encoding.
+        try  {
+            
+            File configFile = new File(config.getConfigDirectory(), Files.COLLECTION_FILENAME);
+            Properties properties = CrawlerConfig.readCollectionConfiguration(configFile.getAbsolutePath(), 
+                searchHome.getAbsolutePath());
+            int verbose = 0;
+            if(log.isDebugEnabled()) {
+                verbose=1;
+            }
+            loadingPolicy.setLoadingRules(properties, verbose, printWriter);
+            boolean accepted = loadingPolicy.acceptableAddress(url);
+            log.debug(new String(bos.toByteArray()));
+            if(accepted) {
+                return MatchResult.MATCHES;
+            } else {
+                return MatchResult.FAILS;
+            }
+        } catch (Throwable t) {
+            log.error(new String(bos.toByteArray())); //will use default encoding.
+            throw t;
+        }
+    }
+    
+}
