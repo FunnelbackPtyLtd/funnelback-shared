d829a5f2ea646233a9c5b0b8cb5592fa37a8fa5c
John Haynes <jhaynes@funnelback.com>
2014-11-25 22:53:20 +0000

    [FUN-7260] - bumping various packages from commons-lang to commons-lang3
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@41157 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/java/com/funnelback/publicui/contentauditor/UrlScopeFill.java
+++ publicui-web/src/main/java/com/funnelback/publicui/contentauditor/UrlScopeFill.java
@@ -18,7 +18,7 @@ import java.util.regex.Pattern;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 
 import com.funnelback.publicui.search.model.collection.facetednavigation.CategoryDefinition;
