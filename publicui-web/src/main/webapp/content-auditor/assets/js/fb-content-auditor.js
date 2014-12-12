fef0efbf28ef37b8d4f61c1d6bccaff6625a36a3
Steve Chan <schan@funnelback.com>
2014-12-12 06:22:50 +0000

    latest commit for content auditor assets
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@41341 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-web/src/main/webapp/content-auditor/assets/js/fb-content-auditor.js
+++ publicui-web/src/main/webapp/content-auditor/assets/js/fb-content-auditor.js
@@ -1,50 +1,41 @@
-// fb-content-auditor.js
+/*
+
+ fb-content-auditor.js
+ 
+*/
+
+
 // Functions
-function createIcons() {
-	/*var target = $('#fb-facets-navigation .nav-stacked .facetLabel');
-		target.find('.fa-circle-thin').remove();
-		target.each(function(key, elem) {
-			$(elem).prepend('<span class="fa fa-circle-thin"></span>&nbsp;');
-		});*/
-}
-
-function cleanFacetsNavigation() {
-	/*	var a = $('#fb-facets-navigation ul li > .facetLabel');
-			if(a.length){
-				
-				var facetFilters = 'contentReportFacetedFilters'; 
-				$('#fb-facet-details h3').after('<div id="' + facetFilters + '" class="col-md-12"><p><strong>Attributes Added<strong></p></div>');
-				
-				
-				$.each(a,function(){
-					if($(this).parents('li').hasClass('active')){$(this).addClass('active');}
-					$(this).appendTo('#'+facetFilters).css('marginBottom','5px');	
-				});
-					$('#' + facetFilters).append(a);
-				}*/
-}
-
 function refreshModalContent() {
 		var modal = $('#modal-overlay .modal-content');
-		//modal.find('style, meta, title, script, head, img').remove();
-		//modal.find('h3, div, td').removeAttr('style');
 		modal.find('table').addClass('table table-hover table-condenesed table-responsive table-striped table-row-clickable');
 	}
-	// jQuery 
+
+// jQuery 
 if (console) console.log('fb-content-auditor.js is initiated.');
 jQuery(function() {
 	$('[data-toggle="tooltip"]').tooltip();
 	$(document)
-		/*.on('click', '.table-row-clickable tbody tr', function() {
-		 	var href = $(this).find('td:first-child a').attr('href');
-		})*/
 		.on('ready', function() {
 			//read the URL hash and open the tab.
 			var hash = window.location.hash;
 			$('a[href="' + hash + '"]').tab('show');
-			//history.pushState("", document.title, window.location.pathname + window.location.search);
-			//window.location.href.substr(0, window.location.href.indexOf('#'));
-			//window.location.href.split('#')[0];
+			
+		})
+		.on('click', '.tab-pane .pagination li', function(){
+			var t = $(this);
+			var pane = t.parents('.tab-pane');
+			var paneId = pane.attr('id');
+			
+			pane.addClass('loading background-danger');
+			if(!t.hasClass('active')){ 
+				var href = t.find('a').attr('href');
+				paneId.load(href + ' #' + paneId , function(){
+					$(this).unwrap();
+				}); 
+			}
+			return false;
+			
 		})
 		.on('click', 'a[href="#collection-1-tab-1"]', function(){
 			chart = $('#fb-facets-navigation ul.nav li.active a').attr('data-chart_ref');
@@ -78,12 +69,7 @@ jQuery(function() {
 			if (t.hasClass('out')) {
 				window.open(href, '_blank');
 			}
-			/*else if(t.hasClass('in')){
-				$.get('/s/anchors-list.html' + href + '&ajax=1', function(data){
-					targetOutput.html(data);
-					refreshModalContent();
-				});
-			}*/
+			
 			else {
 				$.get('/s/anchors.html' + href + '&ajax=1', function(data) {
 				})
@@ -127,7 +113,7 @@ jQuery(function() {
 			$('.navbar-collapse ').toggleClass('in');
 			$('#query').focus();
 		}).on('mouseenter', '#fb-facets-navigation', function() {
-			// $(this).parent('div').removeClass('closed').addClass('opened');
+			// $(this).parent('div').removeClass('closed').addClass('the params that change need to be implemented onto this select box');
 		}).on('mouseleave', '#fb-facets-navigation', function() {
 			// $(this).parent('div').addClass('closed has-animated').removeClass('opened');
 		}).on('focus', '.search-form input', function() {
@@ -144,33 +130,6 @@ jQuery(function() {
 		.on('shown.bs.tab', 'a[data-toggle="tab"]', function(e) {
 			var hash = $(e.target).attr("href").substr(1);
 			window.location.hash = hash;
-			/*//too heavy
-			$.each($('.facet-search-details a') , function(){
-				var href = $(this).attr('href'.split('#')[0])
-				//.replace(/#*$/, '#' + hash)
-				;
-				
-				console.log(href);
-				console.log('#' + hash);
-				$(this).attr('href',href + '#' + hash);
-			});*/
-			
-			
-			//(^|\s)#(\w*[a-zA-Z_]+\w*)
-			
-			
-			/*
-			if(!$('body').attr('data-hash')){
-				$.each($('#my-tab-content .tab-pane'), function(){
-					var newHash =  $(this).attr('id');
-					
-					$.each($(this).find('a'),function(){
-					$(this).attr('href',$(this).attr('href') + '#' + newHash);
-					});
-				});	
-			} else{ }
-			
-			*/
 			$('body').attr('hash', hash);
 			// This is for AMCharts and if the tab element has an attribute of 'data-chart_ref'
 			
@@ -243,11 +202,7 @@ jQuery(function() {
 				'complete': function(e) {
 					target.removeClass('fb-loading').addClass('fb-loading-after');
 					setTimeout(function() {target.removeClass('fb-loading-after');}, 1);
-					createIcons();
-					cleanFacetsNavigation();
 				},
 			});*/
-			//createIcons();
-			//cleanFacetsNavigation();
 		}); //End of Daisy Chain
 });
