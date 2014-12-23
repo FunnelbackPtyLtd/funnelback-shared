54af086d894f2c5457758c439298fd176ff6f5a5
Matthew Sheppard <msheppard@funnelback.com>
2014-12-23 03:18:57 +0000

    Added a security interceptor for content auditor which ensures that it can be accessed only on the admin port (whcih now requires users to log in). Hence, only admin users will be able to access content auditor. For now no per-collection permissions are applied.
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@41460 3cdb6afb-a121-0410-a22c-8da0070c90a1

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/interceptors/ContentAuditorRestrictionInterceptor.java
@@ -0,0 +1,63 @@
+package com.funnelback.publicui.search.web.interceptors;
+
+import java.net.URI;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.funnelback.common.config.Keys;
+import com.funnelback.contentoptimiser.ContentOptimiserUserRestrictions;
+import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
+import com.funnelback.publicui.search.service.ConfigRepository;
+
+/**
+ * Checks if early or late binding DLS is enabled, and then denys access if
+ * either is enabled.
+ */
+public class ContentAuditorRestrictionInterceptor implements
+        HandlerInterceptor {
+
+    @Override
+    public void afterCompletion(HttpServletRequest arg0,
+        HttpServletResponse arg1, Object arg2, Exception arg3)
+        throws Exception {
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+            Object arg2, ModelAndView arg3) throws Exception {
+    }
+
+    @Autowired
+    private ConfigRepository configRepository;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+            HttpServletResponse response, Object handler) throws Exception {
+        // We only allow access on the admin port
+        String adminPort = configRepository.getGlobalConfiguration().value(Keys.Urls.ADMIN_PORT);
+        
+        // Also permit an additional development port (handy if you're running it in eclipse or from maven etc)
+        String additionalAdminPort = configRepository.getGlobalConfiguration().value(Keys.ModernUI.ContentAuditor.ADDITIONAL_PORT);
+        
+        String actualPort = Integer.toString(request.getLocalPort());
+        if (actualPort.equals(adminPort) || actualPort.equals(additionalAdminPort)) {
+            return true;
+        } else {
+            // Redirect them to the right place - The admin version (so they get a login)
+            URI originalUri = new URI(request.getRequestURL().toString());
+            URI redirectUri = new URI("https", originalUri.getUserInfo(), originalUri.getHost(),
+                Integer.parseInt(adminPort), originalUri.getPath(), request.getQueryString(), null);
+                        
+            response.sendRedirect(redirectUri.toString());
+            return false;
+        }
+    }
+
+}
