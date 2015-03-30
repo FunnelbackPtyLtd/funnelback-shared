e862ef2f6b9c7253b7ced8e05f12ad2300eef953
Matthew Sheppard <msheppard@funnelback.com>
2015-03-30 05:13:21 +0000

    Factor out extraction of the faceted navigation related parameters from a search question. Not used yet (will use it in a new Curator trigger, and in place of the source version in the FacetedNavigation input processor).
    git-svn-id: https://svn.cbr.au.funnelback.com/svn/enterprise-search/trunk/java/publicui@42478 3cdb6afb-a121-0410-a22c-8da0070c90a1

--- publicui-core/src/main/java/com/funnelback/publicui/utils/FacetedNavigationUtils.java
+++ publicui-core/src/main/java/com/funnelback/publicui/utils/FacetedNavigationUtils.java
@@ -1,12 +1,22 @@
 package com.funnelback.publicui.utils;
 
-import com.funnelback.common.views.View;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+
+import lombok.extern.log4j.Log4j;
+
 import com.funnelback.common.config.DefaultValues;
 import com.funnelback.common.config.Keys;
+import com.funnelback.common.views.View;
 import com.funnelback.publicui.search.model.collection.Collection;
 import com.funnelback.publicui.search.model.collection.FacetedNavigationConfig;
 import com.funnelback.publicui.search.model.collection.Profile;
+import com.funnelback.publicui.search.model.facetednavigation.FacetParameter;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 
+@Log4j
 public class FacetedNavigationUtils {
 
     /**
@@ -46,6 +56,37 @@ public class FacetedNavigationUtils {
         }
         
         return config;
+    }
+    
+    /**
+     * Returns a set of facet parameters (i.e. CGI parameters for facet selection)
+     * which exist within the SearchQuestion.
+     */
+    public static Set<FacetParameter> getFacetParameters(SearchQuestion searchQuestion) {
+        Set<FacetParameter> result = new HashSet<>();
+        
+        // Read facet names from the 'raw' parameters since they can
+        // be multi-valued (Multiple categories selected for a single facet)
+        MapKeyFilter filter = new MapKeyFilter(searchQuestion.getRawInputParameters());
+        String[] selectedFacetsParams = filter.filter(RequestParameters.FACET_PARAM_PATTERN);
+        
+        if (selectedFacetsParams.length > 0) {
+            for (final String selectedFacetParam: selectedFacetsParams) {
+                log.debug("Found facet parameter '" + selectedFacetParam + "'");
+                Matcher m = RequestParameters.FACET_PARAM_PATTERN.matcher(selectedFacetParam);
+                m.find();
+                
+                final String facetName = m.group(1);
+                final String extraParam = m.group(3);
+                log.debug("Found facet name '" + facetName + "' and extra parameter '" + extraParam + "'");
+                
+                final String values[] = searchQuestion.getRawInputParameters().get(selectedFacetParam);
+                
+                result.add(new FacetParameter(facetName, extraParam, values));
+            }
+        }
+        
+        return result;
     }
     
 }
