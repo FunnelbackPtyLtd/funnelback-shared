fce9d874a25de39dfd578011bc3b0a4cc7e26f30
Matthew Sheppard <msheppard@funnelback.com>
2015-08-17 14:05:37 +1000

    Implemented a special facet type for the duplicate titles which eliminates the 'single' items.

---
+++ publicui-web/src/main/java/com/funnelback/publicui/contentauditor/DuplicatedMetadataFill.java
@@ -0,0 +1,21 @@
+package com.funnelback.publicui.contentauditor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.funnelback.publicui.search.model.collection.facetednavigation.impl.MetadataFieldFill;
+import com.funnelback.publicui.search.model.transaction.Facet.CategoryValue;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+
+/**
+ * A custom metadata fill which is returns only categories with a count greater than one (i.e. duplicates)
+ */
+public class DuplicatedMetadataFill extends MetadataFieldFill {
+
+    @Override
+    public List<CategoryValue> computeValues(final SearchTransaction st) {
+        List<CategoryValue> result = super.computeValues(st);
+        result = result.stream().filter((cv) -> cv.getCount() > 1).collect(Collectors.toList());
+        return result;
+    }
+}
