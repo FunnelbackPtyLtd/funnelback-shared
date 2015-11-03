e22900301ae821ab36b97cdc3870acc2f2d6833d
Luke Butters <lbutters@funnelback.com>
2015-11-04 10:20:00 +1100

    FUN-8075
    Removed the CorsFilter as ConfigurableCorsFilter should work.
    Updated ConfigurableCorsFilter to be more robust to NPE.
    Updated ConfigurableCorsFilter so that if a collection is not given it will use the configuration of global.

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/filters/ConfigurableCorsFilter.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/filters/ConfigurableCorsFilter.java
@@ -11,17 +11,16 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.funnelback.common.config.Keys;
-import com.funnelback.publicui.search.model.collection.Collection;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.service.ConfigRepository;
-import com.funnelback.springmvc.web.filter.AcceptOriginCorsFilter;
+import com.funnelback.springmvc.web.filter.CorsFilter;
 
 /**
  * Filter to add a CORS allow origin header
  * 
  * @author nguillaumin@funnelback.com
  */
-public class ConfigurableCorsFilter extends AcceptOriginCorsFilter implements Filter {
+public class ConfigurableCorsFilter extends CorsFilter implements Filter {
 
     @Autowired
     @Setter
@@ -29,8 +28,16 @@ public class ConfigurableCorsFilter extends AcceptOriginCorsFilter implements Fi
     
     @Override
     public Optional<String> getCorsAllowOrigin(ServletRequest request, ServletResponse response) {
-        Collection c = configRepository.getCollection(request.getParameter(RequestParameters.COLLECTION));
-        return Optional.ofNullable(c.getConfiguration().value(Keys.ModernUI.CORS_ALLOW_ORIGIN));
+        Optional<String> opt = Optional.ofNullable(request.getParameter(RequestParameters.COLLECTION))
+            .map(collectionName -> configRepository.getCollection(collectionName))
+            .map(collection -> collection.getConfiguration())
+            .map(config -> config.value(Keys.ModernUI.CORS_ALLOW_ORIGIN));
+        if(opt.isPresent()){
+            //Try reading from global
+            return opt;
+        }
+        
+        return super.getCorsAllowOrigin(request, response);
     }
 
 }
