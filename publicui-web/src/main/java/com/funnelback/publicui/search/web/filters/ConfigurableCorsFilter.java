f73e9c45cc6855cfc90d68d927a45453f6ae67ee
Luke Butters <lbutters@funnelback.com>
2015-11-03 17:34:06 +1100

    FUN-8075
    colleciton.cfg can set the cors allowed origin.

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/filters/ConfigurableCorsFilter.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/filters/ConfigurableCorsFilter.java
@@ -1,7 +1,19 @@
 package com.funnelback.publicui.search.web.filters;
 
-import javax.servlet.Filter;
+import java.util.Optional;
 
+import javax.servlet.Filter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.funnelback.common.config.Keys;
+import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
+import com.funnelback.publicui.search.service.ConfigRepository;
 import com.funnelback.springmvc.web.filter.AcceptOriginCorsFilter;
 
 /**
@@ -10,5 +22,15 @@ import com.funnelback.springmvc.web.filter.AcceptOriginCorsFilter;
  * @author nguillaumin@funnelback.com
  */
 public class ConfigurableCorsFilter extends AcceptOriginCorsFilter implements Filter {
+
+    @Autowired
+    @Setter
+    private ConfigRepository configRepository;
+    
+    @Override
+    public Optional<String> getCorsAllowOrigin(ServletRequest request, ServletResponse response) {
+        Collection c = configRepository.getCollection(request.getParameter(RequestParameters.COLLECTION));
+        return Optional.ofNullable(c.getConfiguration().value(Keys.ModernUI.CORS_ALLOW_ORIGIN));
+    }
 
 }
