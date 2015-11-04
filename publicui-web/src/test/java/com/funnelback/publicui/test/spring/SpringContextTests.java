259dca5c3304eb3b3604fe835de8f98221181f08
Luke Butters <lbutters@funnelback.com>
2015-11-04 13:29:54 +1100

    Better spring context tests
    I am not sure why the spring context tests are failing, however I have changed those tests so they can be run without needing to set a external dependency. Further the tests search home is isolated from other tests.

--- publicui-web/src/test/java/com/funnelback/publicui/test/spring/SpringContextTests.java
+++ publicui-web/src/test/java/com/funnelback/publicui/test/spring/SpringContextTests.java
@@ -1,38 +1,22 @@
 package com.funnelback.publicui.test.spring;
 
-import java.lang.reflect.Method;
+import java.io.File;
+import java.util.Properties;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.internal.runners.model.ReflectiveCallable;
-import org.junit.internal.runners.statements.ExpectException;
-import org.junit.internal.runners.statements.Fail;
-import org.junit.internal.runners.statements.FailOnTimeout;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.model.FrameworkMethod;
-import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.annotation.ProfileValueUtils;
-import org.springframework.test.annotation.Timed;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContextManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.statements.RunAfterTestClassCallbacks;
-import org.springframework.test.context.junit4.statements.RunAfterTestMethodCallbacks;
-import org.springframework.test.context.junit4.statements.RunBeforeTestClassCallbacks;
-import org.springframework.test.context.junit4.statements.RunBeforeTestMethodCallbacks;
-import org.springframework.test.context.junit4.statements.SpringFailOnTimeout;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.util.ReflectionUtils;
+
+import com.funnelback.common.config.Keys;
+import com.funnelback.common.testutils.SearchHomeConfigs;
+import com.funnelback.common.testutils.SearchHomeFolders;
+import com.funnelback.common.testutils.SearchHomeProvider;
 
 /**
  * A test intended to ensure that the public UI's spring context (the real one, not the test one)
@@ -45,8 +29,47 @@ import org.springframework.util.ReflectionUtils;
     "file:src/main/webapp/WEB-INF/publicui-servlet.xml"
 })
 public class SpringContextTests {
-    // Note - This test expects funnelback.installdir to be set (because the applicationContext.xml spring config does).
-    // Without it it will fail with a NullPointer while trying to construct a file (for searchHome).
+    // These test set and unset funnelback.installdir, it has to use a external class
+    // SpringContextTestHelper to hold what it was set to because it seems static values in this class get reset
+    // when spring gets involved.
+    
+    
+    private static final String f = dosetFunnelbackInstallDir();    
+    
+    private static String dosetFunnelbackInstallDir() {
+        Properties props = System.getProperties();
+        SpringContextTestHelper.ORIG_INSTALL_DIR = props.getProperty("funnelback.installdir");
+        try {
+            SearchHomeConfigs searchHomeConfigs = SearchHomeConfigs.getWithDefaults();
+            searchHomeConfigs.getGlobalCfgDefault().put(Keys.SERVER_SECRET, "foobar");
+            
+            File searchHome = SearchHomeProvider.getWritableSearchHomeIHaveToUseStrings("SpringContextTests", 
+                "testThatContextLoadedSuccessfully", null, searchHomeConfigs, null);
+            new File(SearchHomeFolders.getConfDir(searchHome), "realm.properties").createNewFile();
+            SpringContextTestHelper.WAS_SET = true;
+            props.setProperty("funnelback.installdir", searchHome.getAbsolutePath());
+            
+            File modernUIProps = new File(searchHome, "web/conf/modernui/modernui.properties");
+            modernUIProps.getParentFile().mkdirs();
+            modernUIProps.createNewFile();
+        } catch (Exception e) {
+            throw new RuntimeException("test setup failure, because search home could not be created", e);
+        }
+        
+        return "";
+    }
+    
+    @AfterClass
+    public static void setBackFunnelbackInstallDir() {
+        Properties props = System.getProperties();
+        if(SpringContextTestHelper.WAS_SET) {
+            if(SpringContextTestHelper.ORIG_INSTALL_DIR != null) {
+                props.setProperty("funnelback.installdir", SpringContextTestHelper.ORIG_INSTALL_DIR);
+            } else {
+                props.remove("funnelback.installdir");
+            }
+        }
+    }
     
     /** Mock request provided by the WebAppConfiguration annotation */
     @Autowired
