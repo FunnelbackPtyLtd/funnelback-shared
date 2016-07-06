75ef45afce7b49fb345f5efd2ba45bb0eb8341d2
Luke Butters <lbutters@funnelback.com>
2016-07-06 10:46:22 +1000

    Reloade groovy scripts every min
    FUN-8961
    
    We re-load groovy scripts every 60s to ensure that we get updated dependencies. Without doing this the script itself might not change but stencils it uses may change, however we don't get new version of the stencils while we hold onto the Class.

--- publicui-web/src/main/java/com/funnelback/publicui/search/service/resource/impl/GroovyClassResource.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/service/resource/impl/GroovyClassResource.java
@@ -2,7 +2,6 @@ package com.funnelback.publicui.search.service.resource.impl;
 
 
 import groovy.lang.GroovyClassLoader;
-import groovy.lang.Script;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +13,21 @@ import com.funnelback.springmvc.service.resource.impl.AbstractSingleFileResource
 
 /**
  * Loads and compiles a Groovy class of type T
+ * 
  */
 @Log4j2
 public class GroovyClassResource<T> extends AbstractSingleFileResource<Class<T>> {
     
     private final String collection;
     private final File searchHome;
+   
+    /**
+     * As the dependencies of groovy script can change without us knowing
+     * we don't keep groovy scripts for more than 60s this way at least 
+     * every 60s classes this script might use are reloaded.
+     */
+    static final long MAX_SCRIPT_AGE = 60*1000;
+    
     public GroovyClassResource(File file, String collection, File searchHome) {
         super(file);
         this.collection = collection;
@@ -34,6 +42,28 @@ public class GroovyClassResource<T> extends AbstractSingleFileResource<Class<T>>
         try (GroovyClassLoader cl = GroovyClassLoaderCache.makeNewGroovyClassLoader(searchHome, collection)) {
             return cl.parseClass(file);
         }
+    }
+    
+    
+    @Override
+    public boolean isStale(long timestamp, Class<T> t) {
+        if(isScriptTooOld(timestamp)) {
+            return true;
+        }
+        return super.isStale(timestamp, t);
+    }
+    
+    
+    long getTime() {
+        return System.currentTimeMillis();
+    }
+    
+    
+    boolean isScriptTooOld(long time) {
+        if(getTime() - time > MAX_SCRIPT_AGE) {
+            return true;
+        }
+        return false;
     }
 
 }
