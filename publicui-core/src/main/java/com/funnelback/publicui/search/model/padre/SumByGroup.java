b042554bdfd779977ab9c976edf91192b8ba691f
Luke Butters <lbutters@funnelback.com>
2016-07-21 09:11:27 +1000

    Added more java doc.

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/SumByGroup.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/SumByGroup.java
@@ -8,21 +8,56 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+/**
+ * <p>Holds the sum of numeric metadata class 'on' grouped by the values in metadata class 'by'.</p>
+ * 
+ * @since 15.8
+ */
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class SumByGroup {
     
+    /**
+     * <p>The metadata class we are grouping on.</p>
+     * 
+     * @since 15.8
+     */
     @Getter private final String by;
+    
+    /**
+     * <p>The numeric metadata class we are summing on.</p>
+     * @since 15.8 
+     */
     @Getter private final String on;
     
     @Getter private final List<GroupAndSum> groupAndSums = new ArrayList<>();
  
+    /**
+     * <p>Holds the sum of numeric values in a group</p>
+     * 
+     * @since 15.8
+     */
     @AllArgsConstructor
     @EqualsAndHashCode
     @ToString
     public static class GroupAndSum {
+        
+        /**
+         * <p>The group the sum is for.</p>
+         * 
+         * <p>The group is a single value from the metadata class 
+         * we are grouping on.</p>
+         * 
+         * @since 15.8
+         */
         @Getter private final String group;
+        
+        /**
+         * <p>The sum of all numeric metadata values in the group</p>
+         * 
+         * @since 15.8
+         */
         @Getter private final long sum;
     }
 }
--- publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/UniqueByGroup.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/UniqueByGroup.java
@@ -8,21 +8,55 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+/**
+ * <p>Holds the count of unique values for a metadata class 'on' grouped by the values in metadata class 'by'.</p>
+ * 
+ * @since 15.8
+ */
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class UniqueByGroup {
     
+    /**
+     * <p>The metadata class we are grouping on.</p>
+     * 
+     * @since 15.8
+     */
     @Getter private final String by;
+    
+    /**
+     * <p>The metadata class we are counting unique values for.</p>
+     */
     @Getter private final String on;
     
     @Getter private final List<GroupAndCount> groupAndCounts = new ArrayList<>();
  
+    /**
+     * <p>Holds the count of distinct values in a group.</p> 
+     * 
+     * @since 15.8
+     */
     @AllArgsConstructor
     @EqualsAndHashCode
     @ToString
     public static class GroupAndCount {
+        
+        /**
+         * <p>The group the count is for.</p>
+         * 
+         * <p>The group is a single value from the metadata class 
+         * we are grouping on.</p>
+         * 
+         * @since 15.8
+         */
         @Getter private final String group;
+        
+        /**
+         * <p>The count of unique values in the group.</p>
+         * 
+         * @since 15.8
+         */
         @Getter private final long count;
     }
 }
