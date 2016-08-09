94fc1e2933948e15ab23dd6e1decd923b6aded06
nguillaumin <nguillaumin@funnelback.com>
2016-08-09 14:10:46 -0700

    [FUN-9182] Set a null query if no query is provided

---
+++ publicui-web/src/main/java/com/funnelback/publicui/accessibilityauditor/lifecycle/input/processors/SetQuery.java
@@ -0,0 +1,29 @@
+package com.funnelback.publicui.accessibilityauditor.lifecycle.input.processors;
+
+import org.springframework.stereotype.Component;
+
+import com.funnelback.publicui.search.lifecycle.input.InputProcessorException;
+import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import com.funnelback.publicui.search.model.transaction.SearchTransactionUtils;
+
+/**
+ * Set a default query if none is specified, so that the reports will always
+ * have something to show
+ * 
+ * @author nguillaumin@funnelback.com
+ *
+ */
+@Component("accessibilityAuditorSetQuery")
+public class SetQuery extends AbstractAccessibilityAuditorInputProcessor {
+
+    /** Query to run if no query is specified - should return all results */
+    private static final String NULL_QUERY = "-FunUnusedMetaClass:showalldocuments";
+    
+    @Override
+    public void processAccessibilityAuditorTransaction(SearchTransaction transaction) throws InputProcessorException {
+        if (transaction.getQuestion().getQuery() == null || transaction.getQuestion().getQuery().isEmpty()) {
+            transaction.getQuestion().setQuery(NULL_QUERY);
+        }
+    }
+
+}
