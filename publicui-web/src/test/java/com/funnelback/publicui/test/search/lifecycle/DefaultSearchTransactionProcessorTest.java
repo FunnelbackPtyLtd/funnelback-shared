04a950ba007d6dc44ce4deb66499ee61d09bcf30
nguillaumin <nguillaumin@funnelback.com>
2016-08-10 09:55:52 -0700

    [FUN-9200] Fix error handling (overall + display url processor specific)
    The search transaction processor wasn't handling errors other than the
    one it knew about (input, output, data fetching exceptions), leaving the
    possibility of a processor throwing an exception that would bubble up.
    
    As a result the `error` field of the result packet was never set, and
    since the search controller sets the HTTP response status code depending
    on this field the status code would stay at `200`
    
    Additionally, catch errors directly in `FixDisplayUrls` and output a
    warning if a URL cannot be processed. That may cause issues further down
    but it's already the case because when a transaction goes in error we
    already leave the packet as-is (partially processed). The responsibility
    of checking for errors and handling them is to the template author, I
    guess we could change that if we think that's better but to be done
    separately.

--- publicui-web/src/test/java/com/funnelback/publicui/test/search/lifecycle/DefaultSearchTransactionProcessorTest.java
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/lifecycle/DefaultSearchTransactionProcessorTest.java
@@ -1,17 +1,25 @@
 package com.funnelback.publicui.test.search.lifecycle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.util.StopWatch;
 
 import com.funnelback.publicui.search.lifecycle.DefaultSearchTransactionProcessor;
+import com.funnelback.publicui.search.lifecycle.data.DataFetchException;
 import com.funnelback.publicui.search.lifecycle.data.DataFetcher;
 import com.funnelback.publicui.search.lifecycle.input.InputProcessor;
+import com.funnelback.publicui.search.lifecycle.input.InputProcessorException;
 import com.funnelback.publicui.search.lifecycle.output.OutputProcessor;
+import com.funnelback.publicui.search.lifecycle.output.OutputProcessorException;
 import com.funnelback.publicui.search.model.collection.Collection;
+import com.funnelback.publicui.search.model.transaction.SearchError.Reason;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion;
 import com.funnelback.publicui.search.model.transaction.SearchTransaction;
 import com.funnelback.publicui.test.mock.MockDataFetcher;
@@ -20,25 +28,34 @@ import com.funnelback.publicui.test.mock.MockOutputProcessor;
 
 public class DefaultSearchTransactionProcessorTest {
 
-    @Test
-    public void test() {
-        List<InputProcessor> input = new ArrayList<InputProcessor>();
-        input.add(new MockInputProcessor());
+    private SearchQuestion q;
+    private DefaultSearchTransactionProcessor processor;
+    private List<InputProcessor> input;
+    private List<DataFetcher> data;
+    private List<OutputProcessor> output;
+    
+    @Before
+    public void before() {
+        input = new ArrayList<>();
+        data = new ArrayList<>();
+        output = new ArrayList<>();
         
-        List<OutputProcessor> output = new ArrayList<OutputProcessor>();
-        output.add(new MockOutputProcessor());
-        
-        List<DataFetcher> fetcher = new ArrayList<DataFetcher>();
-        fetcher.add(new MockDataFetcher());
-        
-        DefaultSearchTransactionProcessor processor = new DefaultSearchTransactionProcessor();
+        processor = new DefaultSearchTransactionProcessor();
         processor.setInputFlow(input);
         processor.setOutputFlow(output);
-        processor.setDataFetchers(fetcher);
+        processor.setDataFetchers(data);
         
-        SearchQuestion q = new SearchQuestion();
+        q = new SearchQuestion();
         q.setCollection(new Collection("a-test", null));
         q.setProfile("a-profile");
+
+    }
+    
+    @Test
+    public void test() {
+        input.add(new MockInputProcessor());
+        output.add(new MockOutputProcessor());
+        data.add(new MockDataFetcher());
         
         SearchTransaction st = processor.process(q, null);
         
@@ -58,7 +75,41 @@ public class DefaultSearchTransactionProcessorTest {
         
         Assert.assertTrue(((MockInputProcessor) input.get(0)).isTraversed());
         Assert.assertTrue(((MockOutputProcessor) output.get(0)).isTraversed());
-        Assert.assertTrue(((MockDataFetcher) fetcher.get(0)).isTraversed());
+        Assert.assertTrue(((MockDataFetcher) data.get(0)).isTraversed());
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testErrorHandling() throws Exception {
+        Map<Class<? extends Exception>, Reason> errorReasons = new HashMap<>();
+        errorReasons.put(InputProcessorException.class, Reason.InputProcessorError);
+        errorReasons.put(OutputProcessorException.class, Reason.OutputProcessorError);
+        errorReasons.put(DataFetchException.class, Reason.DataFetchError);
+        errorReasons.put(ObscureException.class, Reason.Unknown);
+        
+        for (Class<Exception> clazz: new Class[] {
+                        InputProcessorException.class,
+                        OutputProcessorException.class,
+                        DataFetchException.class
+        }) {
+            InputProcessor inputProcessor = Mockito.mock(InputProcessor.class);
+            Mockito.doThrow(clazz).when(inputProcessor).processInput(Mockito.any());
+            
+            input.clear();
+            input.add(inputProcessor);
+            
+            SearchTransaction st = processor.process(q, null);
+            
+            Assert.assertNotNull(clazz.getSimpleName() + " error should have been handled", st.getError());
+            Assert.assertEquals("Invalid reason given for " + clazz.getSimpleName(), errorReasons.get(clazz), st.getError().getReason());
+            Assert.assertEquals("Invalid error additional data stored", clazz, st.getError().getAdditionalData().getClass());
+            
+        }
+    }
+    
+    private static class ObscureException extends RuntimeException {
+
+        private static final long serialVersionUID = 1L;
     }
     
 }
