05db8f0bc5fe90d5d9780461f55d8ba1c2fc0116
Matthew Sheppard <msheppard@funnelback.com>
2016-09-02 09:53:47 +1000

    Added a ServletFilterRequestParameterValueExtractor implementation for publicui

---
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/filters/utils/PublicUiServletFilterRequestParameterValueExtractor.java
@@ -0,0 +1,40 @@
+package com.funnelback.publicui.search.web.filters.utils;
+
+import java.util.Optional;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
+import com.funnelback.publicui.search.web.controllers.ResourcesController;
+import com.funnelback.springmvc.utils.web.ServletFilterRequestParameterValueExtractor;
+import com.funnelback.springmvc.utils.web.WebappRequestParameterValueExtractor;
+
+/**
+ * PublicUI specific ServletFilterRequestParameterValueExtractor - Checks both URL params and web resources path params
+ */
+public class PublicUiServletFilterRequestParameterValueExtractor implements ServletFilterRequestParameterValueExtractor {
+
+    /**
+     * Checks both URL params and web resources path params
+     */
+    @Override
+    public Optional<String> getParameterValue(ServletRequest servletRequest, String parameter) {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String collectionId = request.getParameter(RequestParameters.COLLECTION);
+        if (collectionId == null
+                && request.getPathInfo() != null
+                && request.getPathInfo().startsWith(ResourcesController.MAPPING_PATH)
+                && request.getPathInfo().indexOf('/', ResourcesController.MAPPING_PATH.length()) > -1) {
+            collectionId = request.getPathInfo().substring(
+                    ResourcesController.MAPPING_PATH.length(),
+                    request.getPathInfo().indexOf('/', ResourcesController.MAPPING_PATH.length()));
+        }
+        
+        return Optional.of(collectionId);
+    }
+
+}
