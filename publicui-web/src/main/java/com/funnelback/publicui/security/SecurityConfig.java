4078f9e05a1e76836ff41d368f5d764e1c8a26f1
Matthew Sheppard <msheppard@funnelback.com>
2016-10-26 16:25:25 +1100

    Spike for configuring x509 authentication
    This was actually the simple part - we need to change some jetty config and
    work out how to configure the keys and certificates to use.

--- publicui-web/src/main/java/com/funnelback/publicui/security/SecurityConfig.java
+++ publicui-web/src/main/java/com/funnelback/publicui/security/SecurityConfig.java
@@ -1,40 +1,39 @@
 package com.funnelback.publicui.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import com.funnelback.publicui.utils.web.ExecutionContextHolder;
 import com.funnelback.springmvc.api.config.security.ProtectAllHttpBasicAndTokenSecurityConfig;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends ProtectAllHttpBasicAndTokenSecurityConfig {
-    
-    @Autowired
-    ExecutionContextHolder executionContextHolder;
-    
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.
+            inMemoryAuthentication()
+                .withUser("ignoredusername").password("password").roles("USER");
+        // As I understand it, this is required to provide a UserDetailsService, but
+        // we don't actually care about the roles here (because we just care about
+        // whether users were permitted at all or not.
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        switch (executionContextHolder.getExecutionContext()) {
-        case Admin:
-            super.configureHttpbasicAndToken(http);
-            break;
-        case Novell:
-            break;
-        case Public:
-            http
-                .authorizeRequests()
-                .anyRequest()
-                .permitAll();
-            break;
-        case Unknown:
-            break;
-        default:
-            break;
-        }
-        //Disable csrf, as we don't care to protect anything here with it.
-        http.csrf().disable();
+        http.authorizeRequests()
+            .anyRequest().permitAll()
+            .and().x509();
     }
+
+    
 }
