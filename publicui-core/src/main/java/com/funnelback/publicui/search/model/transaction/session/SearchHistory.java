de1f31602db0bac8f105480e09bfb5bb06b83d54
Luke Butters <lbutters@funnelback.com>
2017-01-23 15:52:21 +1100

    Truncate search historu query lengths to padre's max query length.
    FUN-9197
    
    1. Change the DB schema such that query lengths are as long as padre's.
    2. Change the SearchHistory object to auto truncate queries to fit in the DB.
    3. Write a test to ensure the code and DB column lengths match up.

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/session/SearchHistory.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/transaction/session/SearchHistory.java
@@ -9,6 +9,7 @@ import javax.persistence.IdClass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
 
 import com.funnelback.publicui.utils.URLSignature;
 
@@ -20,7 +21,10 @@ import com.funnelback.publicui.utils.URLSignature;
 @Entity
 @IdClass(SearchHistoryPK.class)
 @ToString
+@Log4j2
 public class SearchHistory {
+    
+    public static final int MAX_QUERY_LENGTH = 8196;
 
     /**
      * ID of the User who performed the search
@@ -44,10 +48,32 @@ public class SearchHistory {
     @Getter @Setter private Date searchDate;
     
     /** Original query as entered by the user */
-    @Getter @Setter private String originalQuery;
+    @Getter private String originalQuery;
+    
+    public void setOriginalQuery(String q) {
+        if(q.length() > MAX_QUERY_LENGTH) {
+            //Note that padre actually restricts to MAX_QUERY_LENGTH bytes not chars.
+            log.debug("Shrinking original query to {}", MAX_QUERY_LENGTH);
+            this.originalQuery = q.substring(0, MAX_QUERY_LENGTH);
+        } else {
+            this.originalQuery = q;
+        }
+    }
+    
     
     /** Query as processed by the query processor */
-    @Getter @Setter private String queryAsProcessed;
+    @Getter private String queryAsProcessed;
+    
+    
+    public void setQueryAsProcessed(String q) {
+        if(q.length() > MAX_QUERY_LENGTH) {
+            //Note that padre actually restricts to MAX_QUERY_LENGTH bytes not chars.
+            log.warn("Padre should have truncated the query to {} long it was {} long", MAX_QUERY_LENGTH, q.length());
+            this.queryAsProcessed = q.substring(0, MAX_QUERY_LENGTH);
+        } else {
+            this.queryAsProcessed = q;
+        }
+    }
     
     /** Total number of results returned */
     @Getter @Setter private int totalMatching;
