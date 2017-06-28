0055e8724c5650cc506d4579c23ee4c2ae9eb5a3
Matthew Sheppard <msheppard@funnelback.com>
2017-06-28 11:34:50 +1000

    Account for restoration of the configureGlobal arguments

--- publicui-web/src/main/java/com/funnelback/publicui/security/SecurityConfig.java
+++ publicui-web/src/main/java/com/funnelback/publicui/security/SecurityConfig.java
@@ -120,7 +120,7 @@ public class SecurityConfig extends ProtectAllHttpBasicAndTokenSecurityConfig {
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configureGlobal();
+        super.configureGlobal(auth, funnelbackAdminAuthenticationProvider);
 
         boolean enableSamlAuthentication = configRepository.getGlobalConfiguration().valueAsBoolean(Keys.Auth.PublicUI.SAML.ENABLED, false);
 
