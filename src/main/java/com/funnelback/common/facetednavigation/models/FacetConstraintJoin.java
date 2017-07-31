703f07ffd23dd08f86e1fb44c8094d36d0b16aa6
Luke Butters <lbutters@funnelback.com>
2017-07-31 10:35:20 +1000

    Added doco about the facet type enums

--- src/main/java/com/funnelback/common/facetednavigation/models/FacetConstraintJoin.java
+++ src/main/java/com/funnelback/common/facetednavigation/models/FacetConstraintJoin.java
@@ -1,7 +1,20 @@
 package com.funnelback.common.facetednavigation.models;
 
+/**
+ * Defines how selected constraints within a facet are applied.
+ *
+ */
 public enum FacetConstraintJoin {
+    /**
+     * Requires that all selected constraints are meet.  
+     */
     AND,
+    /**
+     * Requires that at least on of the selected constraints are meet.
+     */
     OR,
+    /**
+     * Constraints are joined in the same way as version 15.10 and before did.
+     */
     LEGACY;
 }
