dc9d428262973a459b592fb8e1bff0d190fb12c7
Luke Butters <lbutters@funnelback.com>
2017-07-31 10:35:20 +1000

    Added doco about the facet type enums

--- src/main/java/com/funnelback/common/facetednavigation/models/FacetSelectionType.java
+++ src/main/java/com/funnelback/common/facetednavigation/models/FacetSelectionType.java
@@ -1,6 +1,18 @@
 package com.funnelback.common.facetednavigation.models;
 
+/**
+ * Defines how facet selection works.
+ *
+ */
 public enum FacetSelectionType {
+    /**
+     * When a category value is selected other category values of equal or lower
+     * depth are unselected.
+     */
     SINGLE,
+    
+    /**
+     * When a category value is selected other category values are left selected.
+     */
     MULTIPLE;
 }
--- src/main/java/com/funnelback/common/facetednavigation/models/FacetValues.java
+++ src/main/java/com/funnelback/common/facetednavigation/models/FacetValues.java
@@ -1,6 +1,22 @@
 package com.funnelback.common.facetednavigation.models;
 
+/**
+ * Defines where the (category) values to display in facet come from.
+ *
+ */
 public enum FacetValues {
+    /**
+     * Values are from the query which may have facets applied.
+     * <p>If you had selected `red cars` then (if each car has one colour)
+     * you would not see facet values for any other car colour.</p>
+     */
     FROM_SCOPED_QUERY,
+    
+    /**
+     * Values are from the unscoped query, that is the user's query without
+     * any facets selected.
+     * <p>If you had selected `red cars` then you would still see the colours of
+     * other cars.</p>s
+     */
     FROM_UNSCOPED_QUERY;
 }
