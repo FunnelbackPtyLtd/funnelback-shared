a0243cb8a5d787ae88f94b4c47a0f39ad46de602
Liliana Nowak <lnowak@funnelback.com>
2017-08-02 10:04:47 +1000

    Updated trigger to use profile/collection config; Fixed unit tests

--- publicui-core/src/test/java/com/funnelback/publicui/test/search/model/curator/trigger/QueryRegularExpressionTriggerTests.java
+++ publicui-core/src/test/java/com/funnelback/publicui/test/search/model/curator/trigger/QueryRegularExpressionTriggerTests.java
@@ -1,39 +1,47 @@
 package com.funnelback.publicui.test.search.model.curator.trigger;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import java.io.FileNotFoundException;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.funnelback.common.config.Config;
-import com.funnelback.common.config.DefaultValues;
-import com.funnelback.common.config.Keys;
 import com.funnelback.common.system.EnvironmentVariableException;
+import com.funnelback.config.configtypes.service.DefaultServiceConfig;
+import com.funnelback.config.configtypes.service.ServiceConfig;
+import com.funnelback.config.data.InMemoryConfigData;
+import com.funnelback.config.data.environment.NoConfigEnvironment;
+import com.funnelback.config.keys.Keys.FrontEndKeys;
 import com.funnelback.publicui.search.model.collection.Collection;
-import com.funnelback.publicui.search.model.curator.trigger.ExactQueryTrigger;
+import com.funnelback.publicui.search.model.collection.Profile;
 import com.funnelback.publicui.search.model.curator.trigger.QueryRegularExpressionTrigger;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion;
 import com.funnelback.publicui.search.model.transaction.SearchTransaction;
+import com.google.common.collect.Maps;
 
 public class QueryRegularExpressionTriggerTests {
 
+    private static final String COLLECTION_ID = "test-collection";
+    private static final String PROFILE_NAME = "profileName";
+    private SearchQuestion question;
+
+    @Before
+    public void before() throws Exception {
+        Profile profile = new Profile();
+        profile.setServiceConfig(new DefaultServiceConfig(new InMemoryConfigData(Maps.newHashMap()), new NoConfigEnvironment()));
+        Collection collection = new Collection(COLLECTION_ID, null);
+        collection.getProfiles().put(PROFILE_NAME, profile);
+
+        question = new SearchQuestion();
+        question.setCollection(collection);
+        question.setCurrentProfile(PROFILE_NAME);
+    }
+
     @Test
     public void testSimpleCases() {
-        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
-        
-        SearchQuestion question = new SearchQuestion();
-        
-        Config config = mock(Config.class);
-        when(config.value(Keys.ModernUI.Curator.QUERY_PARAMETER_PATTERN, 
-            DefaultValues.ModernUI.Curator.QUERY_PARAMETER_PATTERN))
-            .thenReturn(DefaultValues.ModernUI.Curator.QUERY_PARAMETER_PATTERN);
-        question.setCollection(new Collection("test-collection", config));
-        
         SearchTransaction st = new SearchTransaction(question, null);
         
+        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
         qret.setTriggerPattern("(?i)\\bFoo\\b");
 
         question.setQuery("Food");
@@ -45,18 +53,9 @@ public class QueryRegularExpressionTriggerTests {
 
     @Test
     public void testInvalidRegex() {
-        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
-        
-        SearchQuestion question = new SearchQuestion();
-        
-        Config config = mock(Config.class);
-        when(config.value(Keys.ModernUI.Curator.QUERY_PARAMETER_PATTERN, 
-            DefaultValues.ModernUI.Curator.QUERY_PARAMETER_PATTERN))
-            .thenReturn(DefaultValues.ModernUI.Curator.QUERY_PARAMETER_PATTERN);
-        question.setCollection(new Collection("test-collection", config));
-        
         SearchTransaction st = new SearchTransaction(question, null);
         
+        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
         qret.setTriggerPattern("(?i)\\b(Foo\\b");
 
         question.setQuery("foo");
@@ -65,18 +64,20 @@ public class QueryRegularExpressionTriggerTests {
 
     @Test
     public void testMultiParamRegularExpressionTrigger() throws EnvironmentVariableException, FileNotFoundException {
-        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
-        
-        SearchQuestion question = new SearchQuestion();
-        
-        Config config = mock(Config.class);
-        when(config.value(Keys.ModernUI.Curator.QUERY_PARAMETER_PATTERN, 
-            DefaultValues.ModernUI.Curator.QUERY_PARAMETER_PATTERN))
-            .thenReturn("^query.*");
-        question.setCollection(new Collection("test-collection", config));
-        
+        ServiceConfig serviceConfig = new DefaultServiceConfig(new InMemoryConfigData(Maps.newHashMap()), new NoConfigEnvironment());
+        serviceConfig.set(FrontEndKeys.UI.Modern.Curator.QUERY_PARAMETER_PATTERN, "^query.*");
+        Profile profile = new Profile();
+        profile.setServiceConfig(serviceConfig);
+        Collection collection = new Collection(COLLECTION_ID, null);
+        collection.getProfiles().put(PROFILE_NAME, profile);
+
+        question = new SearchQuestion();
+        question.setCollection(collection);
+        question.setCurrentProfile(PROFILE_NAME);
+
         SearchTransaction st = new SearchTransaction(question, null);
         
+        QueryRegularExpressionTrigger qret = new QueryRegularExpressionTrigger();
         qret.setTriggerPattern("(?i)\\bo\\sS\\b");
 
         question.getInputParameterMap().put("query_or", "O");
