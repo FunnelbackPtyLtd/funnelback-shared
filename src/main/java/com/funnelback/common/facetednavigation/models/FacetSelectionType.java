e2c53d45973fa4373e49f5e5dbacb74db149747e
Luke Butters <lbutters@funnelback.com>
2017-08-04 08:53:08 +1000

    Add enum for unselecting other facets.

--- src/main/java/com/funnelback/common/facetednavigation/models/FacetSelectionType.java
+++ src/main/java/com/funnelback/common/facetednavigation/models/FacetSelectionType.java
@@ -6,6 +6,11 @@ package com.funnelback.common.facetednavigation.models;
  */
 public enum FacetSelectionType {
     /**
+     * Unselects other facets when selected, useful for tab type facets.
+     * 
+     */
+    SINGLE_AND_UNSELECT_OTHER_FACETS,
+    /**
      * When a category value is selected other category values of equal or lower
      * depth are unselected.
      */
