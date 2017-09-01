b35eff28fde69dc9737c03bbe43bfa406bc86aa8
Luke Butters <lbutters@funnelback.com>
2017-09-01 10:11:43 +1000

    Un ignore the test.

--- publicui-core/src/test/java/com/funnelback/publicui/search/model/transaction/facet/order/FacetComparatorProviderTest.java
+++ publicui-core/src/test/java/com/funnelback/publicui/search/model/transaction/facet/order/FacetComparatorProviderTest.java
@@ -1,6 +1,10 @@
 package com.funnelback.publicui.search.model.transaction.facet.order;
 
-import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.*;
+import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.COUNT_ASCENDING;
+import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.COUNT_DESCENDING;
+import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.LABEL_ASCENDING;
+import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.LABEL_DESCENDING;
+import static com.funnelback.common.facetednavigation.models.FacetValuesOrder.SELECTED_FIRST;
 import static java.util.Arrays.asList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -10,12 +14,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.funnelback.common.facetednavigation.models.FacetValuesOrder;
 import com.funnelback.publicui.search.model.transaction.Facet.CategoryValue;
-@Ignore
+
 public class FacetComparatorProviderTest {
     
     // These are made global otherwise we need to overridde the equals and I am too 
