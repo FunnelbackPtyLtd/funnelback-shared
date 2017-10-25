06e15de5bb6100503312f4f12fc09c23cf7c79a2
Luke Butters <lbutters@funnelback.com>
2017-10-25 12:19:04 +1100

    Use another delimiter
    because XML 1.0 wont work with ascii char 29 and not everything support XML 1.1.

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/collection/facetednavigation/impl/DateFieldFill.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/collection/facetednavigation/impl/DateFieldFill.java
@@ -1,6 +1,5 @@
 package com.funnelback.publicui.search.model.collection.facetednavigation.impl;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -42,14 +41,15 @@ public class DateFieldFill extends CategoryDefinition implements MetadataBasedCa
     /**
      * This separates the query constraint e.g. d=2016 from the label e.g. "2016"
      * 
-     * The separator must be one that does not appear in either query constraint
-     * or label.
+     * The separator must be one that does not appear in the query constraint
      * 
-     * We use ascii 29 which is "group separator" seems like an ok choice I doubt
-     * anything will be tripped up by it and it is unlikely it will be in the query
-     * constraint or label.
+     * We can not use ascii control chars like 29 (group separator) because XML 1.0 will throw a fit
+     * and not all programs work with XML 1.1 for example xmllint. Instead we will just make a separator
+     * that I hope will not be in the constraint.
+     *
+     * 
      */
-    public static final String CONSTRAINT_AND_LABEL_SEPERATOR = new String(new byte[]{29, 29}, StandardCharsets.UTF_8);
+    public static final String CONSTRAINT_AND_LABEL_SEPERATOR = " :: ";
     
     private static final Pattern CONSTRAINT_AND_LABEL_SEPERATOR_PATTERN = Pattern.compile(CONSTRAINT_AND_LABEL_SEPERATOR);
     
