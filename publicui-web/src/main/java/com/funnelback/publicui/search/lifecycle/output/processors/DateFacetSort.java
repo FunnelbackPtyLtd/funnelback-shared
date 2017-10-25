20b762fa227010d715fc564123ec0bc1c08561a2
Luke Butters <lbutters@funnelback.com>
2017-10-25 14:50:28 +1100

    Respect the collection.cfg date sort option
    FUN-10955
    
    We respect the option while the facet is a legacy one.
    
    The model is update to say that it is only sorted by date.

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/DateFacetSort.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/DateFacetSort.java
@@ -1,14 +1,16 @@
 package com.funnelback.publicui.search.lifecycle.output.processors;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 import org.springframework.stereotype.Component;
-
-import lombok.extern.log4j.Log4j2;
 
 import com.funnelback.common.config.DefaultValues;
 import com.funnelback.common.config.DefaultValues.FacetedNavigation.DateSortMode;
 import com.funnelback.common.config.Keys;
+import com.funnelback.common.facetednavigation.models.FacetConstraintJoin;
+import com.funnelback.common.facetednavigation.models.FacetValuesOrder;
 import com.funnelback.publicui.search.lifecycle.output.AbstractOutputProcessor;
 import com.funnelback.publicui.search.lifecycle.output.OutputProcessorException;
 import com.funnelback.publicui.search.model.collection.FacetedNavigationConfig;
@@ -17,10 +19,13 @@ import com.funnelback.publicui.search.model.collection.facetednavigation.FacetDe
 import com.funnelback.publicui.search.model.collection.facetednavigation.impl.DateFieldFill;
 import com.funnelback.publicui.search.model.transaction.Facet;
 import com.funnelback.publicui.search.model.transaction.Facet.Category;
-import com.funnelback.publicui.search.model.transaction.facet.order.ByDateComparator;
 import com.funnelback.publicui.search.model.transaction.SearchTransaction;
 import com.funnelback.publicui.search.model.transaction.SearchTransactionUtils;
+import com.funnelback.publicui.search.model.transaction.facet.order.ByDateComparator;
+import com.funnelback.publicui.search.model.transaction.facet.order.FacetComparatorProvider;
 import com.funnelback.publicui.utils.FacetedNavigationUtils;
+
+import lombok.extern.log4j.Log4j2;
 
 /**
  * Sort date based facets by chronological order
@@ -53,7 +58,16 @@ public class DateFacetSort extends AbstractOutputProcessor {
                     return;
                 }
                 
-                final ByDateComparator comparator = new ByDateComparator(DateSortMode.valueOf(sortMode));
+                
+                // Set the order
+                FacetValuesOrder order = FacetValuesOrder.DATE_ASCENDING;
+                if(DateSortMode.valueOf(sortMode) == DateSortMode.ddate) {
+                    order = FacetValuesOrder.DATE_DESCENDING;
+                }
+                
+                final Comparator<Facet.CategoryValue> comparator = 
+                        new FacetComparatorProvider()
+                            .getComparatorWhenSortingValuesFromSingleCategory(Arrays.asList(order));
                 
                 // Each facet
                 for(FacetDefinition facetDef: config.getFacetDefinitions()) {
@@ -63,12 +77,16 @@ public class DateFacetSort extends AbstractOutputProcessor {
                             // Date based category, find corresponding values
                             // in the response
                             for (Facet facet: searchTransaction.getResponse().getFacets()) {
-                                if (facet.getName().equals(facetDef.getName())) {
-                                    for (Category cat: facet.getCategories()) {
-                                        if (cat.getQueryStringParamName().equals(catDef.getQueryStringParamName())) {
-                                            log.debug("Sorting category '"+cat.getQueryStringParamName()
-                                                +"' for facet '"+facet.getName()+"'");
-                                            sortCategory(cat, comparator);
+                                if(facet.getConstraintJoin() == FacetConstraintJoin.LEGACY) {
+                                    if (facet.getName().equals(facetDef.getName())) {
+                                        
+                                        for (Category cat: facet.getCategories()) {
+                                            if (cat.getQueryStringParamName().equals(catDef.getQueryStringParamName())) {
+                                                log.debug("Sorting category by date '"+cat.getQueryStringParamName()
+                                                    +"' for facet '"+facet.getName()+"', ordering set on the facet will be disabled");
+                                                sortCategory(cat, comparator);
+                                                facet.setOrder(Arrays.asList(order));
+                                            }
                                         }
                                     }
                                 }
@@ -85,7 +103,7 @@ public class DateFacetSort extends AbstractOutputProcessor {
      * @param category {@link Category} to sort
      * @param comparator Comparator to use for sorting
      */
-    private void sortCategory(Category category, ByDateComparator comparator) {
+    private void sortCategory(Category category, Comparator<Facet.CategoryValue> comparator) {
         Collections.sort(category.getValues(), comparator);
         // Recursively sort sub-categories
         for (Category c: category.getCategories()) {
