64115ab4298be01ab5fdefc0bb8ca9fa0c034c3c
Luke Butters <lbutters@funnelback.com>
2017-11-08 06:59:17 +1100

    Add a few more validation checks to the jsonp callback param
    FUN-10561
    
    We reject any whitespace and reject any control chars.
    
    This also allows `"` as that can be used when calling a function on a array `array_of_functions["key"]`.

--- publicui-web/src/main/java/com/funnelback/publicui/utils/JsonPCallbackParam.java
+++ publicui-web/src/main/java/com/funnelback/publicui/utils/JsonPCallbackParam.java
@@ -6,6 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+/**
+ * This limits what the Jsonp callback can be 
+ * 
+ * <p>The only way in which a JsonP callback can be exploited is if the original
+ * page itself has a bug which allows the user to set the callback. What is done 
+ * here helps prevent such their security flaw from being exploited with our
+ * end-points. Further pen tests claim reflected JS and HTML as a flaw although
+ * technically not because you need something else to interpret the response in
+ * correctly or do the above, we block some suspicious requests rather than 
+ * explain why they are wrong.</p>
+ *
+ */
 @NoArgsConstructor
 @Log4j2
 public class JsonPCallbackParam {
@@ -21,7 +33,12 @@ public class JsonPCallbackParam {
     
     
     public static boolean isValid(String callback) {
-        return !CharMatcher.anyOf("<>/\\\"'()").matchesAnyOf(callback);
+        // Must not be important HTML or java script chars.
+        return !CharMatcher.anyOf("<>/\\()' \t,;{}").matchesAnyOf(callback)
+            // MUst not be tricky control chars.
+            && !CharMatcher.inRange((char) 0, (char) 31).matchesAnyOf(callback)
+            // Must not have white space (although this does mean a callback of foo["a b"] is made invalid
+            && !CharMatcher.whitespace().matchesAnyOf(callback);
     }
     
     @Override
