a75ebb4289bf1c78fcb195347dc71ec17111e0c7
Luke Butters <lbutters@funnelback.com>
2017-11-15 14:10:09 +1100

    On windows only use the pure java implemention otherwise try to use the C implementations

--- publicui-web/src/main/java/com/funnelback/publicui/utils/InputSupplyingLZ4OuputStream.java
+++ publicui-web/src/main/java/com/funnelback/publicui/utils/InputSupplyingLZ4OuputStream.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.function.Supplier;
+import java.util.zip.Checksum;
+
+import org.apache.commons.exec.OS;
 
 import lombok.AllArgsConstructor;
 import net.jpountz.lz4.LZ4BlockInputStream;
@@ -15,7 +18,37 @@ import net.jpountz.xxhash.XXHashFactory;
 
 public class InputSupplyingLZ4OuputStream implements InputSupplyingOuputStream {
 
+    /**
+     * Same seed used by lz4 java library
+     */
     static final int DEFAULT_SEED = 0x9747b28c;
+    
+    private static final LZ4Factory COMPRESSOR_FACTORY = getLZ4CompressorFactory();
+    
+    private static LZ4Factory getLZ4CompressorFactory() {
+        // Don't use the fastest c implementations on windows as the files will not be cleaned up
+        // although maybe it will because jetty has its own tmp dir
+        if(OS.isFamilyWindows()) {
+            return LZ4Factory.fastestJavaInstance();
+        }
+        return LZ4Factory.fastestInstance();
+    }
+    
+    private static final XXHashFactory XX_HASH_FACTORY = getXXHashFactory();
+    
+    private static XXHashFactory getXXHashFactory() {
+        // Don't use the fastest c implementations on windows as the files will not be cleaned up
+        // although maybe it will because jetty has its own tmp dir
+        if(OS.isFamilyWindows()) {
+            return XXHashFactory.fastestJavaInstance();
+        }
+        return XXHashFactory.fastestInstance();
+    }
+    
+    private static Checksum getChecksum() {
+        return XX_HASH_FACTORY.newStreamingHash32(DEFAULT_SEED).asChecksum();
+    }
+    
     
     private ByteArrayOutputStream bos;
     private OutputStream os;
@@ -24,9 +57,7 @@ public class InputSupplyingLZ4OuputStream implements InputSupplyingOuputStream {
 
     public InputSupplyingLZ4OuputStream(ByteArrayOutputStream bos) throws IOException {
         this.bos = bos;
-        this.os = new LZ4BlockOutputStream(bos, 1 << 16, 
-            LZ4Factory.fastestJavaInstance().fastCompressor(),
-            XXHashFactory.fastestJavaInstance().newStreamingHash32(DEFAULT_SEED).asChecksum(), false);
+        this.os = new LZ4BlockOutputStream(bos, 1 << 16, COMPRESSOR_FACTORY.fastCompressor(), getChecksum(), false);
     }
 
     /**
@@ -44,35 +75,42 @@ public class InputSupplyingLZ4OuputStream implements InputSupplyingOuputStream {
         
         @Override
         public InputStream get() {
-            return new LZ4BlockInputStream(new ByteArrayInputStream(bytes));
+            return new LZ4BlockInputStream(new ByteArrayInputStream(bytes), COMPRESSOR_FACTORY.fastDecompressor(), getChecksum());
         }
         
     }
 
+    @Override
     public void write(int b) throws IOException {
         os.write(b);
     }
 
+    @Override
     public int hashCode() {
         return os.hashCode();
     }
 
+    @Override
     public void write(byte[] b) throws IOException {
         os.write(b);
     }
 
+    @Override
     public void write(byte[] b, int off, int len) throws IOException {
         os.write(b, off, len);
     }
 
+    @Override
     public boolean equals(Object obj) {
         return os.equals(obj);
     }
 
+    @Override
     public void flush() throws IOException {
         os.flush();
     }
 
+    @Override
     public void close() throws IOException {
         os.close();
     }
