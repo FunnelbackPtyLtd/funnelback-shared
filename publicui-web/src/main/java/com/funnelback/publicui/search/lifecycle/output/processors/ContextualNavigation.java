ca432300854ee1ca687e77de14082700f65d383f
Matthew Sheppard <msheppard@funnelback.com>
2018-01-19 11:05:20 +1100

    Prevent query paramters set by the modern UI (such as gscope1 and origin) from being exposed explicitly in contextual navigation links.
    See: SUPPORT-2580

--- publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/ContextualNavigation.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/lifecycle/output/processors/ContextualNavigation.java
@@ -1,8 +1,10 @@
 package com.funnelback.publicui.search.lifecycle.output.processors;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
@@ -10,14 +12,17 @@ import com.funnelback.publicui.search.lifecycle.output.AbstractOutputProcessor;
 import com.funnelback.publicui.search.lifecycle.output.OutputProcessorException;
 import com.funnelback.publicui.search.model.padre.Category;
 import com.funnelback.publicui.search.model.padre.Cluster;
+import com.funnelback.publicui.search.model.padre.ClusterNav;
 import com.funnelback.publicui.search.model.transaction.SearchQuestion.RequestParameters;
 import com.funnelback.publicui.search.model.transaction.SearchTransaction;
 import com.funnelback.publicui.search.model.transaction.SearchTransactionUtils;
 import com.funnelback.publicui.utils.QueryStringUtils;
+import com.google.common.collect.Sets;
 
 /**
  * Process PADRE contextual navigation response:
- * - Clears up "more_link"s to remove the hardcoded script name /search/padre-sw.cgi
+ * - Cleans up links from padre (removing outdated paths, irrelevant parameters)
+ * - Adds tacking information for logging purposes
  * 
  *
  */
@@ -30,17 +35,21 @@ public class ContextualNavigation extends AbstractOutputProcessor {
                 && searchTransaction.getResponse().hasResultPacket()
                 && searchTransaction.getResponse().getResultPacket().getContextualNavigation() != null) {
             
+            ClusterNav clusterNav = searchTransaction.getResponse().getResultPacket().getContextualNavigation().getClusterNav();
+            clusterNav.setUrl(cleanContextualNavigationLink(searchTransaction.getQuestion().getQueryStringMapCopy(), clusterNav.getUrl()));
+            
             for (Category category: searchTransaction.getResponse().getResultPacket().getContextualNavigation().getCategories()) {
-                // Strips any hardcoded URL such as "/search/padre-sw.cgi?..."
                 if (category.getMoreLink() != null) {
-                    category.setMoreLink(category.getMoreLink().substring(category.getMoreLink().indexOf("?")));
+                    category.setMoreLink(cleanContextualNavigationLink(searchTransaction.getQuestion().getQueryStringMapCopy(), category.getMoreLink()));
                 }
                 if (category.getFewerLink() != null) {
-                    category.setFewerLink(category.getFewerLink().substring(category.getFewerLink().indexOf("?")));
+                    category.setFewerLink(cleanContextualNavigationLink(searchTransaction.getQuestion().getQueryStringMapCopy(), category.getFewerLink()));
                 }
                 
                 // Sets additional parameters to the URL for logging purposes
                 for (Cluster cluster: category.getClusters()) {
+                    cluster.setHref(cleanContextualNavigationLink(searchTransaction.getQuestion().getQueryStringMapCopy(), cluster.getHref()));
+                    
                     Map<String, List<String>> qs = QueryStringUtils.toMap(cluster.getHref());
 
                     // Remove repeated parameters from PADRE
@@ -78,6 +87,41 @@ public class ContextualNavigation extends AbstractOutputProcessor {
             }
         }
 
+    }
+    
+    private final static Set<String> PADRE_CONTEXTUAL_NAVIGATION_QUERY_PARAMETERS_TO_KEEP = Sets.newHashSet("site_max_clusters", "topic_max_clusters", "type_max_clusters", "query");
+    
+    /**
+     * Cleans up a padre-generated contextual navigation link for modern-ui usage. In particular it will...
+     * 
+     * - Remove anything before the first '?' if one is present (because padre may start links 
+     *   with /search/padre-sw.cgi?...). If no '?' is present, the return value will not have one either.
+     *   
+     * - Keep only parameters that are set in the original userQueryStringMapCopy or that are specifically set by padre
+     *   (see PADRE_CONTEXTUAL_NAVIGATION_QUERY_PARAMTERS_TO_KEEP). This avoids double-applying a gscope which a facet
+     *   causes to be set, but which is passed to padre via the query_string.
+     */
+    public /* private if not for unit tests */ static String cleanContextualNavigationLink(Map<String, List<String>> userQueryStringMapCopy, String padreGeneratedLink) {
+        Map<String, List<String>> contextualNavigationLinkQueryStringMap;
+        Boolean prependQuestionMark;
+        if (padreGeneratedLink.contains("?")) {
+            contextualNavigationLinkQueryStringMap = QueryStringUtils.toMap(padreGeneratedLink.substring(padreGeneratedLink.indexOf("?") + 1));
+            prependQuestionMark = true;
+        } else {
+            contextualNavigationLinkQueryStringMap = QueryStringUtils.toMap(padreGeneratedLink);
+            prependQuestionMark = false;            
+        }
+
+        Map<String, List<String>> resultMap = new HashMap<>();
+        resultMap.putAll(userQueryStringMapCopy);
+
+        for (String param : PADRE_CONTEXTUAL_NAVIGATION_QUERY_PARAMETERS_TO_KEEP) {
+            if (contextualNavigationLinkQueryStringMap.containsKey(param)) {
+                resultMap.put(param, contextualNavigationLinkQueryStringMap.get(param));
+            }
+        }
+
+        return (QueryStringUtils.toString(resultMap, prependQuestionMark));
     }
 
 }
