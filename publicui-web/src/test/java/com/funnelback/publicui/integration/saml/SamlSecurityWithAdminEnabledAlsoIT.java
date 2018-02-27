bc0accde97735c67699e2b70bb1c2a7a5480ec6f
Matthew Sheppard <msheppard@funnelback.com>
2018-02-27 14:57:07 +1100

    Include the ports in the subclass, make the test purpose clearer

--- publicui-web/src/test/java/com/funnelback/publicui/integration/saml/SamlSecurityWithAdminEnabledAlsoIT.java
+++ publicui-web/src/test/java/com/funnelback/publicui/integration/saml/SamlSecurityWithAdminEnabledAlsoIT.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.springframework.util.SocketUtils;
 
 import com.funnelback.springmvc.utils.saml.MujinaIdentityProviderServer;
 import com.funnelback.springmvc.utils.security.DefaultSecurityConfiguredJettyServer;
@@ -17,24 +18,25 @@ public class SamlSecurityWithAdminEnabledAlsoIT extends SamlSecurityIT {
     public static void startServers() throws Exception {
         SamlSecurityIT.mujina = new MujinaIdentityProviderServer();
         SamlSecurityIT.mujina.start();
+        
+        Integer port = SocketUtils.findAvailableTcpPort();
+        File searchHome = createSearchHome(port);
 
-        File searchHome = createSearchHome();
-
+        // The test is basically that none of these settings matter, because we're the public publicui in this case
         File globalCfgDefault = new File(searchHome, "conf" + File.separator + "global.cfg.default");
         Files.append("\n", globalCfgDefault, StandardCharsets.UTF_8);
         Files.append("auth.admin.saml.enabled=true\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.identity-provider-metadata-url=http://localhost:" + mujina.getPort() + "/metadata\n", globalCfgDefault, StandardCharsets.UTF_8);
+        Files.append("auth.admin.saml.identity-provider-metadata-url=http://not-a-real-place.com/\n", globalCfgDefault, StandardCharsets.UTF_8);
         Files.append("auth.admin.saml.entity-id-prefix=com:funnelback:admin:sp\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.keystore-path=" + new File("src/test/resources/saml/samlKeystore.jks").getAbsolutePath() + "\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.keystore-password=nalle123\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.key-alias=apollo\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.enabled=true\n", globalCfgDefault, StandardCharsets.UTF_8);
-        Files.append("auth.admin.saml.key-password=nalle123\n", globalCfgDefault, StandardCharsets.UTF_8);
+        Files.append("auth.admin.saml.keystore-path=does-not-exist\n", globalCfgDefault, StandardCharsets.UTF_8);
+        Files.append("auth.admin.saml.keystore-password=foo\n", globalCfgDefault, StandardCharsets.UTF_8);
+        Files.append("auth.admin.saml.key-alias=bar\n", globalCfgDefault, StandardCharsets.UTF_8);
+        Files.append("auth.admin.saml.key-password=bar\n", globalCfgDefault, StandardCharsets.UTF_8);
         Files.append("auth.admin.saml.groovy-permission-mapper=doesntmattercauseitshouldnotberead.groovy\n", globalCfgDefault, StandardCharsets.UTF_8);
         
         SamlSecurityIT.searchHome = searchHome;
         
-        SamlSecurityIT.server = new DefaultSecurityConfiguredJettyServer(searchHome, "/s");
+        SamlSecurityIT.server = new DefaultSecurityConfiguredJettyServer(searchHome, "/s", port);
         SamlSecurityIT.server.start();
     }
 
