e228e0193471e0d7234fdab48a4440163f546dce
Liliana Nowak <lnowak@funnelback.com>
2018-03-06 16:25:56 +1100

    Replaced reference to metamap.cfg and xml.cfg with metadata classes

--- publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/Result.java
+++ publicui-core/src/main/java/com/funnelback/publicui/search/model/padre/Result.java
@@ -134,17 +134,17 @@ public class Result implements ResultType {
     @Getter @Setter private String exploreLink;
     
     /**
-     * Distance in kilometres of this search result
+     * Distance in kilometers of this search result
      * from the origin set when the query is run.
      */
     @Getter @Setter private Float kmFromOrigin;
     
     /**
-     * <p>Map containing the metadata fields of the results.</p>
+     * <p>Map containing the metadata fields for the result.</p>
      * 
-     * <p>The key is the letter the metadata is mapped on.</p>
+     * <p>The key is the metadata class name as defined in the metadata mappings.</p>
      * 
-     * @see <code>metamap.cfg</code>, <code>xml.cfg</code>
+     * @see <code>Metadata classes</code>
      */
     @Getter private final Map<String, String> metaData = new HashMap<String, String>();
     
