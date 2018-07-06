bf3d48ea1e67ccaaa1d2459f3df05d620525be5d
Matthew Sheppard <msheppard@funnelback.com>
2018-07-06 14:06:13 +1000

    Simplify the key listing (assuming we'll always have string keys).

--- publicui-web/src/main/java/com/funnelback/publicui/streamedresults/datafetcher/MultimapDynamicPropertyHandler.java
+++ publicui-web/src/main/java/com/funnelback/publicui/streamedresults/datafetcher/MultimapDynamicPropertyHandler.java
@@ -1,9 +1,5 @@
 package com.funnelback.publicui.streamedresults.datafetcher;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.commons.jxpath.DynamicPropertyHandler;
 
 import com.google.common.collect.Multimap;
@@ -11,14 +7,8 @@ import com.google.common.collect.Multimap;
 public class MultimapDynamicPropertyHandler implements DynamicPropertyHandler {
 
     public String[] getPropertyNames(Object object) {
-        Multimap map = (Multimap) object;
-        Set set = map.keySet();
-        String[] names = new String[set.size()];
-        Iterator it = set.iterator();
-        for (int i = 0; i < names.length; i++) {
-            names[i] = String.valueOf(it.next());
-        }
-        return names;
+        Multimap<String, ?> map = (Multimap) object;
+        return map.keySet().toArray(new String[]{});
     }
 
     public Object getProperty(Object object, String propertyName) {
