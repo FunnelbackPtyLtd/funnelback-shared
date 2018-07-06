bc1b46604f93292a4e5a63efd83f396c72d35da0
Matthew Sheppard <msheppard@funnelback.com>
2018-07-06 14:42:33 +1000

    Added @Override annotations requested in MR.

--- publicui-web/src/main/java/com/funnelback/publicui/streamedresults/datafetcher/MultimapDynamicPropertyHandler.java
+++ publicui-web/src/main/java/com/funnelback/publicui/streamedresults/datafetcher/MultimapDynamicPropertyHandler.java
@@ -6,15 +6,18 @@ import com.google.common.collect.Multimap;
 
 public class MultimapDynamicPropertyHandler implements DynamicPropertyHandler {
 
+    @Override
     public String[] getPropertyNames(Object object) {
         Multimap<String, ?> map = (Multimap) object;
         return map.keySet().toArray(new String[]{});
     }
 
+    @Override
     public Object getProperty(Object object, String propertyName) {
         return ((Multimap) object).get(propertyName);
     }
 
+    @Override
     public void setProperty(Object object, String propertyName, Object value) {
         ((Multimap) object).put(propertyName, value);
     }
