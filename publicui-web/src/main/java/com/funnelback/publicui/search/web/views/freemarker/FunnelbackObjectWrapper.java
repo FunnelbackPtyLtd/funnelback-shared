ff5c4c054f6a88ab8af004fa2304275574279835
Matthew Sheppard <msheppard@funnelback.com>
2018-07-25 16:22:32 +1000

    Add some doc, avoid using two different object wrappers in case it matters.

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/views/freemarker/FunnelbackObjectWrapper.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/views/freemarker/FunnelbackObjectWrapper.java
@@ -9,28 +9,43 @@ import freemarker.template.MapKeyValuePairIterator;
 import freemarker.template.ObjectWrapper;
 import freemarker.template.SimpleCollection;
 import freemarker.template.TemplateCollectionModel;
-import freemarker.template.TemplateHashModel;
 import freemarker.template.TemplateHashModelEx2;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 import freemarker.template.WrappingTemplateModel;
-import freemarker.template.TemplateHashModelEx2.KeyValuePairIterator;
 
+/**
+ * Custom object wrapper which knows how to handle any special object
+ * types that Funnelback uses.
+ */
 public class FunnelbackObjectWrapper extends DefaultObjectWrapper {
 
     public FunnelbackObjectWrapper() {
         super(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+        // That default is apparently to maintain compatibility with freemarker 2.3.0.
+        // For now we'll keep doing that because I think it's what we had before.
+        //
+        // It might be valuable to investigate what the changes are and whether
+        // they are things that would be worth more than whatever pain
+        // switching to them might cause...Someday.
     }
 
     @Override
     protected TemplateModel handleUnknownType(final Object obj) throws TemplateModelException {
         if (obj instanceof ListMultimap) {
-            return new ListMultimapAdapter((ListMultimap<String, ?>) obj, this);
+            // We only support String keyed maps - Freemarker wouldn't work with anything else anyway I think.
+            @SuppressWarnings("unchecked")
+            ListMultimap<String, ?> stringKeyedMultimap = (ListMultimap<String, ?>) obj;
+            return new ListMultimapAdapter(stringKeyedMultimap, this);
         }
-
+        
         return super.handleUnknownType(obj);
     }
 
+    /**
+     * An adapter which allows Freemarker to treat Guava ListMulitmaps as if they
+     * were a Map<String, List<Object>>.
+     */
     public class ListMultimapAdapter extends WrappingTemplateModel implements TemplateHashModelEx2, AdapterTemplateModel {
 
         private final ListMultimap<String, ?> multimap;
