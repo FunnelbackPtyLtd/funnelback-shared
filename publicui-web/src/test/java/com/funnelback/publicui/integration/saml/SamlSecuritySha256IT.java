c9e84b443b6ff7d2435075871db86f0e7780d04f
Matthew Sheppard <msheppard@funnelback.com>
2019-05-08 12:12:38 +1000

    Added a test that setting sha256 really does make us sign things that way

--- publicui-web/src/test/java/com/funnelback/publicui/integration/saml/SamlSecuritySha256IT.java
+++ publicui-web/src/test/java/com/funnelback/publicui/integration/saml/SamlSecuritySha256IT.java
@@ -48,27 +48,27 @@ import com.funnelback.springmvc.utils.saml.MujinaIdentityProviderServer;
 import com.funnelback.springmvc.utils.saml.TokenUtils;
 import com.funnelback.springmvc.utils.security.DefaultSecurityConfiguredJettyServer;
 
-public class SamlSecurityIT {
+public class SamlSecuritySha256IT {
     protected static DefaultSecurityConfiguredJettyServer server;
     protected static MujinaIdentityProviderServer mujina;
     protected static File searchHome;
 
     @BeforeClass
     public static void startServers() throws Exception {
-        SamlSecurityIT.mujina = new MujinaIdentityProviderServer();
-        SamlSecurityIT.mujina.start();
+        SamlSecuritySha256IT.mujina = new MujinaIdentityProviderServer();
+        SamlSecuritySha256IT.mujina.start();
 
         Integer port = SocketUtils.findAvailableTcpPort();
         searchHome = createSearchHome(port);
 
-        SamlSecurityIT.server = new DefaultSecurityConfiguredJettyServer(searchHome, "/s", port);
-        SamlSecurityIT.server.start();
+        SamlSecuritySha256IT.server = new DefaultSecurityConfiguredJettyServer(searchHome, "/s", port);
+        SamlSecuritySha256IT.server.start();
     }
 
     public static File createSearchHome() throws Exception, IOException {
         return createSearchHome(0);
     }
-    
+
     public static File createSearchHome(Integer port) throws Exception, IOException {
         SearchHomeConfigs searchHomeConfigs = SearchHomeConfigs.getWithDefaults();
         searchHomeConfigs.getGlobalCfgDefault().put("server_secret", "test");
@@ -79,101 +79,31 @@ public class SamlSecurityIT {
         searchHomeConfigs.getGlobalCfgDefault().put("auth.publicui.saml.keystore-password", "nalle123");
         searchHomeConfigs.getGlobalCfgDefault().put("auth.publicui.saml.key-alias", "apollo");
         searchHomeConfigs.getGlobalCfgDefault().put("auth.publicui.saml.key-password", "nalle123");
+        searchHomeConfigs.getGlobalCfgDefault().put("auth.saml.algorithm", "sha256");
         searchHomeConfigs.getGlobalCfgDefault().put("urls.search_port", port.toString());
 
-        File searchHome = SearchHomeProvider.getNamedWritableSearchHomeForTestClass(SamlSecurityIT.class, searchHomeConfigs, "SAML-Server");
-        
+        File searchHome = SearchHomeProvider.getNamedWritableSearchHomeForTestClass(SamlSecuritySha256IT.class, searchHomeConfigs, "SAML-Server");
+
         DefaultSecurityConfiguredJettyServer.basicSearchHomeSetupForServer(searchHome);
         DefaultAdminSecurityIT.commonPublicUISearchHomeSetup(searchHome);
-        
+
         return searchHome;
-    }
-
-    @Test
-    public void testProactiveHttpBasic() throws Exception {
-        CloseableHttpClient httpclient = HttpClients.custom().build();
-
-        HttpGet get = new HttpGet(server.getBaseUrl() + "search.html");
-        get.addHeader("Authorization", "Basic YWRtaW46YWRtaW4="); // That's admin:admin
-
-        try (CloseableHttpResponse response = httpclient.execute(get)) {
-            HttpEntity entity = response.getEntity();
-
-            String responseText = IOUtils.toString(entity.getContent());
-            EntityUtils.consume(entity);
-            
-            Assert.assertThat("Expected to reach the Funnelback collection listing page after proactive HTTP Basic authentication", responseText,
-                StringContains.containsString("Access granted!"));
-        }
-
-        HttpGet get2 = new HttpGet(server.getBaseUrl() + "search.html");
-        // Do it again with no authorization header now - The JSESSIONID cookie should suffice
-
-        try (CloseableHttpResponse response = httpclient.execute(get2)) {
-            HttpEntity entity = response.getEntity();
-
-            String responseText = IOUtils.toString(entity.getContent());
-            EntityUtils.consume(entity);
-
-            Assert.assertThat("Expected to get the collection listing page with a JSESSIONID cookie", responseText,
-                StringContains.containsString("Access granted!"));
-        }
-    }
-
-    @Test
-    public void testTokenAuth() throws Exception {
-        CloseableHttpClient httpclient = HttpClients.custom().build();
-        
-        new SharedBetweenContainersHelper()
-            .getSharedConcurrentHashMap(SecurityConfigBase.USER_SALT_MAP_KEY)
-                .put("admin", "dummyHash");
-        String token = TokenUtils.makeToken(searchHome, "test", "admin", "dummyHash");
-        
-        HttpGet get = new HttpGet(server.getBaseUrl() + "search.html");
-        get.addHeader("X-Security-Token", token); // That's admin:admin
-
-        try (CloseableHttpResponse response = httpclient.execute(get)) {
-            HttpEntity entity = response.getEntity();
-
-            String responseText = IOUtils.toString(entity.getContent());
-            EntityUtils.consume(entity);
-            
-            Assert.assertThat("Expected to get the collection listing page with an X-Security-Token header", responseText,
-                StringContains.containsString("Access granted!"));
-        }
     }
 
     @Test
     public void testSaml() throws IOException {
         try (Response response = MujinaIdentityProviderServer.testSamlLogin(
             server, mujina, server.getBaseUrl() + "search.html",
-            "http://www.w3.org/2000/09/xmldsig#rsa-sha1")) {
+            "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256")) {
             Assert.assertThat("Expected to reach the Funnelback collection listing page after SAML authentication",
                 response.body().string(),
                 StringContains.containsString("Access granted!"));
         }
     }
 
-    @Test
-    public void testFetchMetadata() throws Exception {
-        CloseableHttpClient httpclient = HttpClients.custom().build();
-
-        HttpGet get = new HttpGet(server.getBaseUrl() + "saml/metadata");
-
-        try (CloseableHttpResponse response = httpclient.execute(get)) {
-            HttpEntity entity = response.getEntity();
-
-            String responseText = IOUtils.toString(entity.getContent());
-            EntityUtils.consume(entity);
-            
-            Assert.assertThat("Expected to get the SAML metadata", responseText,
-                StringContains.containsString("X509Certificate"));
-        }
-    }
-    
     @AfterClass
     public static void stopServers() throws Exception {
-        SamlSecurityIT.server.stop();
-        SamlSecurityIT.mujina.stop();
+        SamlSecuritySha256IT.server.stop();
+        SamlSecuritySha256IT.mujina.stop();
     }
 }
