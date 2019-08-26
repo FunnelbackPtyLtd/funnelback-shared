536d3d6ea695992f4f9b154bd183927e4a8718d9
Matthew Sheppard <msheppard@funnelback.com>
2019-08-26 16:05:56 +1000

    Removed unused import

--- publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/SearchController.java
+++ publicui-web/src/main/java/com/funnelback/publicui/search/web/controllers/SearchController.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import freemarker.core.HackHtmlEscapingBuiltIn;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
