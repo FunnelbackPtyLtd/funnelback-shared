03699a6c76643d53068410b6e27fd253452390ff
Luke Butters <lbutters@funnelback.com>
2019-11-13 13:12:38 +1100

    Quicklinks now uses query_processor_options from profile.cfg FUN-13401

--- publicui-web/src/test/java/com/funnelback/publicui/test/search/lifecycle/input/processors/QuickLinksTests.java
+++ publicui-web/src/test/java/com/funnelback/publicui/test/search/lifecycle/input/processors/QuickLinksTests.java
@@ -1,9 +1,18 @@
 package com.funnelback.publicui.test.search.lifecycle.input.processors;
 
+import com.funnelback.config.configtypes.service.ServiceConfigReadOnly;
 import com.funnelback.config.keys.collection.QuickLinkKeys;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
 
 import org.junit.Test;
+
+import java.io.File;
 
 import com.funnelback.common.system.EnvironmentVariableException;
 import com.funnelback.common.config.Keys;
@@ -17,6 +26,8 @@ import com.funnelback.publicui.search.model.transaction.SearchTransaction;
 public class QuickLinksTests {
 
     private QuickLinks processor = new QuickLinks();
+    
+    private File searchHome = new File("src/test/resources/dummy-search_home");
     
     @Test
     public void testMissingData() throws InputProcessorException {
@@ -34,13 +45,17 @@ public class QuickLinksTests {
     }
     
     @Test
-    public void testWithConfig() throws InputProcessorException, EnvironmentVariableException {
-        Collection c = new Collection("dummy", new NoOptionsConfig("dummy"));
+    public void testWithConfig() throws Exception {
+        Collection c = new Collection("dummy", new NoOptionsConfig(searchHome, "dummy"));
         c.getConfiguration().setValue(QuickLinkKeys.PREFIX, "enabled");
         c.getConfiguration().setValue(com.funnelback.config.keys.Keys.CollectionKeys.QuickLinkKeys.DEPTH.getKey(), "42");
         c.getConfiguration().setValue(QuickLinkKeys.PREFIX + ".rank", "666");
         
-        SearchQuestion question = new SearchQuestion();
+        SearchQuestion question = spy(new SearchQuestion());
+        ServiceConfigReadOnly profileConfig = mock(ServiceConfigReadOnly.class);
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("");
+        doReturn(profileConfig).when(question).getCurrentProfileConfig();
+        
         question.setCollection(c);
 
         SearchTransaction st = new SearchTransaction(question, null);
@@ -51,13 +66,13 @@ public class QuickLinksTests {
         
         // Try with conflicting query processor options
         st.getQuestion().getDynamicQueryProcessorOptions().clear();
-        c.getConfiguration().setValue(Keys.QUERY_PROCESSOR_OPTIONS, "-stem=2 -QL=4 -res=xml -QL_rank=all -something");
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("-stem=2 -QL=4 -res=xml -QL_rank=all -something");
         processor.processInput(st);
         Assert.assertEquals(0, st.getQuestion().getDynamicQueryProcessorOptions().size());
 
         // Try with non-conflicting QP options
         st.getQuestion().getDynamicQueryProcessorOptions().clear();
-        c.getConfiguration().setValue(Keys.QUERY_PROCESSOR_OPTIONS, "-stem=2 -res=xml -something");
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("-stem=2 -res=xml -something");
         processor.processInput(st);
         Assert.assertEquals(2, st.getQuestion().getDynamicQueryProcessorOptions().size());
         Assert.assertTrue(st.getQuestion().getDynamicQueryProcessorOptions().contains("-QL=42"));
@@ -65,12 +80,16 @@ public class QuickLinksTests {
     }
     
     @Test
-    public void testWithoutConfig() throws InputProcessorException, EnvironmentVariableException {
-        Collection c = new Collection("dummy", new NoOptionsConfig("dummy"));
+    public void testWithoutConfig() throws Exception {
+        Collection c = new Collection("dummy", new NoOptionsConfig(searchHome, "dummy"));
         c.getConfiguration().setValue(QuickLinkKeys.PREFIX, "enabled");
         c.getConfiguration().setValue(QuickLinkKeys.PREFIX + ".rank", "1");
         
-        SearchQuestion question = new SearchQuestion();
+        SearchQuestion question = spy(new SearchQuestion());
+        ServiceConfigReadOnly profileConfig = mock(ServiceConfigReadOnly.class);
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("");
+        doReturn(profileConfig).when(question).getCurrentProfileConfig();
+        
         question.setCollection(c);
 
         SearchTransaction st = new SearchTransaction(question, null);
@@ -81,13 +100,13 @@ public class QuickLinksTests {
         
         // Try with conflicting query processor options
         st.getQuestion().getDynamicQueryProcessorOptions().clear();
-        c.getConfiguration().setValue(Keys.QUERY_PROCESSOR_OPTIONS, "-stem=2 -QL=4 -res=xml -QL_rank=all -something");
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("-stem=2 -QL=4 -res=xml -QL_rank=all -something");
         processor.processInput(st);
         Assert.assertEquals(0, st.getQuestion().getDynamicQueryProcessorOptions().size());
 
         // Try with non-conflicting QP options
         st.getQuestion().getDynamicQueryProcessorOptions().clear();
-        c.getConfiguration().setValue(Keys.QUERY_PROCESSOR_OPTIONS, "-stem=2 -res=xml -something");
+        when(profileConfig.get(com.funnelback.config.keys.Keys.FrontEndKeys.QueryProcessor.QUERY_PROCESSOR_OPTIONS)).thenReturn("-stem=2 -res=xml -something");
         processor.processInput(st);
         Assert.assertEquals(2, st.getQuestion().getDynamicQueryProcessorOptions().size());
         Assert.assertTrue(st.getQuestion().getDynamicQueryProcessorOptions().contains("-QL=1"));
