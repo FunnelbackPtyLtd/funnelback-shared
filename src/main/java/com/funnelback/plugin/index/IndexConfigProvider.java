37f7e806e6ba001beb2348fc2e57531671fdd9e7
Luke Butters <lbutters@funnelback.com>
2020-02-25 17:04:05 +1100

    Provide facets from its own interface.
    FUN-13696
    
    Rather than from the index config provider as I don't think people think facets are index configuration.

--- src/main/java/com/funnelback/plugin/index/IndexConfigProvider.java
+++ src/main/java/com/funnelback/plugin/index/IndexConfigProvider.java
@@ -146,46 +146,6 @@ public interface IndexConfigProvider {
     }
     
     /**
-     * Supply additional faceted navigation configuration.
-     * 
-     * Additional faceted navigation is supplied as a JSON similar to the API
-     * <code>
-     * GET /faceted-navigation/v2/collections/{collection}/profiles/{profile}/facet/{id}/views/{view}
-     * <code>
-     * 
-     * This expects to return a list <code>[]</code> of Facets. The id,
-     * lastModified and created fields do not need to be set.
-     * 
-     * Since the acceptable combinations of facets is complicated, it may be easier 
-     * to design the facets you want using the faceted navigation UI. After configuring 
-     * your facet, extract the facet configuration JSON from the preview screen and omit the 
-     * id, lastModified and created fields.
-     * 
-     * For example:
-     * <code>
-     * [{
-     * "name": "Authors",
-     * "facetValues": "FROM_SCOPED_QUERY_HIDE_UNSELECTED_PARENT_VALUES",
-     * "constraintJoin": "AND",
-     * "selectionType": "SINGLE",
-     * "categories": [{ "type": "MetaDataFieldCategory", "subCategories": [], "metadataField": "author" }],
-     * "order": [ "SELECTED_FIRST", "COUNT_DESCENDING"]
-     * }]
-     * </code>
-     * 
-     * During query processing each facet must have a unique name. Facets defined on the profile
-     * will be used in preference to facets defined from the plugin. After that facets from plugins will
-     * be used so long as the facet, by name, does not already exist.
-     * 
-     * 
-     * @return JSON as a string which contains a list of facet definitions as returned by the
-     * API. 
-     */
-    public default String extraFacetedNavigation(IndexConfigProviderContext context) {
-        return null;
-    }
-    
-    /**
      * Supply query completion CSV files to use on profiles.
      * 
      * The QueryCompletionCSV supports defining a single CSV "file" for many profiles. The
