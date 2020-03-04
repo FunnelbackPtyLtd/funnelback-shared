ef814af0a3b291e4366b655c1e3b403d958cd1ac
Yasith Lokuge <ylokuge@yasith-lokuges-macbook-pro.local>
2020-03-04 16:40:31 +1100

    review changes added

--- plugin-archetypes/src/main/resources/META-INF/archetype-post-generate.groovy
+++ plugin-archetypes/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -4,19 +4,21 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.SimpleFileVisitor
 import java.nio.file.StandardCopyOption
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.BasicFileAttributes
+@Grab(group='org.apache.commons', module='commons-text', version='1.8')
+import org.apache.commons.text.WordUtils
 
 // the path where the project got generated
 Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
 
 // the properties available to the archetype
-Properties properties = request.properties
+properties = request.properties
 
 // the Java package of the generated project, e.g. com.acme
-String groupId = request.groupId
+groupId = request.groupId
 
 // convert it into a path, e.g. com/acme
-String packagePath = groupId.replace(".", "/")
+packagePath = groupId.replace(".", "/")
 
 boolean isGathererEnabled = Boolean.parseBoolean(properties.get("gatherer"))
 boolean isIndexingEnabled = Boolean.parseBoolean(properties.get("indexing"))
@@ -24,42 +26,30 @@ boolean isFacetsEnabled = Boolean.parseBoolean(properties.get("facets"))
 boolean isSearchLifeCycleEnabled = Boolean.parseBoolean(properties.get("searchLifeCycle"))
 
 // Remove non alpha numeric chars
-String pluginPrefix = request.artifactId.replaceAll("[^a-zA-Z0-9]","")
+pluginPrefix = request.artifactId.replaceAll("[^a-zA-Z0-9]"," ")
 
-// make the first letter capitalized for the class name
-String pluginClassPrefix = pluginPrefix.substring(0, 1).toUpperCase() + pluginPrefix.substring(1)
+// make the first letter of each word capitalized for the class name
+pluginClassPrefix = WordUtils.capitalizeFully(pluginPrefix).replaceAll(" ", "")
 
-Path target = projectPath.resolve(request.version + "/src/main/java/" + packagePath)
-Path tmp = projectPath.resolve(request.version + "/tmp")
-Path resources = projectPath.resolve(request.version + "/src/main/resources")
-File propertiesFile = resources.resolve("funnelback-plugin-" + request.artifactId + ".properties").toFile()
+target = projectPath.resolve(request.version + "/src/main/java/" + packagePath)
+tmp = projectPath.resolve(request.version + "/tmp")
+resources = projectPath.resolve(request.version + "/src/main/resources")
+propertiesFile = resources.resolve("funnelback-plugin-" + request.artifactId + ".properties").toFile()
 
 if(isGathererEnabled) {
-    String gathererPlugin = "CustomGatherPlugin.java";
-    Path gatherer = tmp.resolve(gathererPlugin)
-    Files.copy(gatherer, target.resolve(pluginClassPrefix + gathererPlugin), StandardCopyOption.REPLACE_EXISTING)
-    propertiesFile.append("com.funnelback.plugin.gatherer.GathererPlugin=" + groupId + "." + pluginClassPrefix + "CustomGatherPlugin\n")
+    enableImplementation("CustomGatherPlugin", "com.funnelback.plugin.gatherer.GathererPlugin")
 }
 
 if(isIndexingEnabled) {
-    String indexingPlugin = "IndexingPlugin.java";
-    Path gatherer = tmp.resolve(indexingPlugin)
-    Files.copy(gatherer, target.resolve(pluginClassPrefix + indexingPlugin), StandardCopyOption.REPLACE_EXISTING)
-    propertiesFile.append("com.funnelback.plugin.index.IndexingConfigProvider=" + groupId + "." + pluginClassPrefix + "IndexingPlugin\n")
+    enableImplementation("IndexingPlugin", "com.funnelback.plugin.index.IndexingConfigProvider")
 }
 
 if(isFacetsEnabled) {
-    String facetsPlugin = "FacetsPlugin.java";
-    Path gatherer = tmp.resolve(facetsPlugin)
-    Files.copy(gatherer, target.resolve(pluginClassPrefix + facetsPlugin), StandardCopyOption.REPLACE_EXISTING)
-    propertiesFile.append("com.funnelback.plugin.facets.FacetProvider=" + groupId + "." + pluginClassPrefix + "FacetsPlugin\n")
+    enableImplementation("FacetsPlugin", "com.funnelback.plugin.facets.FacetProvider")
 }
 
 if(isSearchLifeCycleEnabled) {
-    String searchLifeCyclePlugin = "SearchLifeCyclePlugin.java";
-    Path gatherer = tmp.resolve(searchLifeCyclePlugin)
-    Files.copy(gatherer, target.resolve(pluginClassPrefix + searchLifeCyclePlugin), StandardCopyOption.REPLACE_EXISTING)
-    propertiesFile.append("com.funnelback.plugin.SearchLifeCyclePlugin=" + groupId + "." + pluginClassPrefix + "SearchLifeCyclePlugin\n")
+    enableImplementation("SearchLifeCyclePlugin", "com.funnelback.plugin.SearchLifeCyclePlugin")
 }
 
 // Delete tmp directory and files
@@ -79,3 +69,10 @@ Files.walkFileTree(tmp, new SimpleFileVisitor<Path>() {
         return FileVisitResult.CONTINUE
     }
 })
+
+def enableImplementation(String impl, String qualifiedInterface) {
+    String className = impl + ".java"
+    Path source = tmp.resolve(className)
+    Files.copy(source, target.resolve(pluginClassPrefix + className), StandardCopyOption.REPLACE_EXISTING)
+    propertiesFile.append(qualifiedInterface + "=" + groupId + "." + pluginClassPrefix + impl + "\n")
+}
