782ce8d43be76de520b99dce2163eb2d08354c2c
Luke Butters <lbutters@funnelback.com>
2020-03-10 12:53:50 +1100

    doco

--- plugin-shared-test/src/main/java/com/funnelback/filter/api/mock/UnknownDocumentTypeFactory.java
+++ plugin-shared-test/src/main/java/com/funnelback/filter/api/mock/UnknownDocumentTypeFactory.java
@@ -3,6 +3,13 @@ package com.funnelback.filter.api.mock;
 import com.funnelback.filter.api.DocumentType;
 import com.funnelback.filter.api.DocumentTypeFactory;
 
+/**
+ * A document type factory which always returns {@link DocumentType#MIME_UNKNOWN}.
+ * 
+ * The {@link DocumentTypeFactory} that is provided under Funnelback e.g. during a crawl
+ * will provide some support for creating a DocumentType from the Content-Type HTTP header.  
+ *
+ */
 public class UnknownDocumentTypeFactory implements DocumentTypeFactory {
 
     @Override
