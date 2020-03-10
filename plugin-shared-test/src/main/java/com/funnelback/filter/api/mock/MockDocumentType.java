bbaa0051ddc34e85c71ed3189cec428454510e8b
Luke Butters <lbutters@funnelback.com>
2020-03-10 12:49:37 +1100

    Doco

--- plugin-shared-test/src/main/java/com/funnelback/filter/api/mock/MockDocumentType.java
+++ plugin-shared-test/src/main/java/com/funnelback/filter/api/mock/MockDocumentType.java
@@ -10,13 +10,28 @@ import lombok.Setter;
 import lombok.With;
 
 
+/**
+ * Use this to create a DocumentType for testing when a preexisting DocumentType on
+ * the interface {@link DocumentType} doesn't already exist.
+ * 
+ * Example creating a DocumentType that claims to be HTML with a specific value for
+ * what is returned by {@link DocumentType#asContentType()}. 
+ * <code>
+ * new MockDocumentType().withHTML(true).withContentType("something/specific");
+ * </code>
+ * 
+ */
 @Setter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class MockDocumentType implements DocumentType {
 
+    /**
+     * Construct a DocumentType where the cotent type is unknown.
+     * 
+     */
     public MockDocumentType() {
-        this(false, false, false, DocumentType.MIME_UNKNOWN.asContentType()); 
+        this(false, false, false, DocumentType.MIME_UNKNOWN.asContentType());
     }
     
     @Getter @With public boolean HTML;
