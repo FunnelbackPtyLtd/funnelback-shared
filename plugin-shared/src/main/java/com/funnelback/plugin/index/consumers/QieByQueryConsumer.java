feece9bd80d93d75873957696267d1c0abc5b1d4
Hailun Tan <htan@squiz.net>
2023-08-21 23:23:21 +1000

    FUN-16828: address reviewer's comments.

--- plugin-shared/src/main/java/com/funnelback/plugin/index/consumers/QieByQueryConsumer.java
+++ plugin-shared/src/main/java/com/funnelback/plugin/index/consumers/QieByQueryConsumer.java
@@ -3,7 +3,7 @@ package com.funnelback.plugin.index.consumers;
 public interface QieByQueryConsumer {
 
     /**
-     * A plugin may call this to supply a gscope which should be set for all documents that match the query.
+     * A plugin may call this to supply a QIE which should be set for all documents that match the query.
      * 
      * @param qieWeight The QIE weight to be set. range: 0 - 1.
      * @param query The query which will be run.
--- plugin-shared/src/main/java/com/funnelback/plugin/index/consumers/QieByUrlConsumer.java
+++ plugin-shared/src/main/java/com/funnelback/plugin/index/consumers/QieByUrlConsumer.java
@@ -3,7 +3,7 @@ package com.funnelback.plugin.index.consumers;
 public interface QieByUrlConsumer {
 
     /**
-     * A plugin may call this to supply a qie which should be set to the given URLs.
+     * A plugin may call this to supply a qie which should be set to the given URL(s).
      * 
      * @param qieWeight The QIE weight to be set. range: 0 - 1.
      * @param url The URL whose QIE weight is set.
