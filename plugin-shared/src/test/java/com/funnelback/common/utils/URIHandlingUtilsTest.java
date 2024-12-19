e26a548586581484143e3b79ff20fbded6a6ee51
Hailun Tan <htan@squiz.net>
2024-12-19 03:56:08 +0000

    [SEARCH-720] Add the port number and user credentials for the URI constructionist URI utils

--- plugin-shared/src/test/java/com/funnelback/common/utils/URIHandlingUtilsTest.java
+++ plugin-shared/src/test/java/com/funnelback/common/utils/URIHandlingUtilsTest.java
@@ -18,8 +18,12 @@ public class URIHandlingUtilsTest {
 
         Assertions.assertEquals("www.example.com", URIHandlingUtils.create("www.example.com").toString());
 
+        Assertions.assertEquals("http://www.example.com:8080", URIHandlingUtils.create("http://www.example.com:8080").toString());
+
+        Assertions.assertEquals("http://admin:pass@example.com:99/docUrl#test", URIHandlingUtils.create("http://admin:pass@example.com:99/docUrl#test").toString());
+
         // It will always encode the "|" but not the other special characters accepted in URI, such as "&",  "?", ";", "/", ":" or "#" if the target string is an absolute URL
-        Assertions.assertEquals("http://www.example.com/test?;docUrl=http://example.com#test%7C&1", URIHandlingUtils.create("http://www.example.com/test?;docUrl=http://example.com#test|&1").toString());
+        Assertions.assertEquals("http://admin:pass@example.com:99/test?;docUrl=http://admin:pass@example.com#test%7C&1", URIHandlingUtils.create("http://admin:pass@example.com:99/test?;docUrl=http://admin:pass@example.com#test|&1").toString());
 
         // It will encode all the special characters if the target string  is not an absolute URL
         Assertions.assertEquals("test%3F%3BdocUrl%3Dhttp%3A%2F%2Fexample.com%23test%7C%261", URIHandlingUtils.create("test?;docUrl=http://example.com#test|&1").toString());
